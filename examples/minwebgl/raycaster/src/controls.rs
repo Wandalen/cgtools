1aa6939fbe4e36f389d675e7775a5df0cfc370c5
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-10-28 20:33:51 +0200

    wip

---
+++ examples/minwebgl/raycaster/src/controls.rs
@@ -0,0 +1,72 @@
+use std::{ cell::RefCell, rc::Rc };
+use web_sys::wasm_bindgen::prelude::*;
+
+#[ derive( Default ) ]
+pub struct Controls
+{
+  forward : bool,
+  backward : bool,
+  left : bool,
+  right : bool,
+}
+
+impl Controls
+{
+  pub fn as_vec( &self ) -> [ f32; 2 ]
+  {
+    [
+      self.forward as i32 as f32 - self.backward as i32 as f32,
+      self.left as i32 as f32 - self.right as i32 as f32,
+    ]
+  }
+
+  pub fn setup() -> Rc< RefCell< Self > >
+  {
+    let this = Self::default();
+    let this = Rc::new( RefCell::new( this ) );
+    let window = web_sys::window().expect( "Window should exist" );
+
+    let on_keydown : Closure< dyn Fn( _ ) > = Closure::new
+    (
+      {
+        let this = this.clone();
+        move | event : web_sys::KeyboardEvent |
+        {
+          match event.code().as_str()
+          {
+            "KeyW" => this.borrow_mut().forward = true,
+            "KeyA" => this.borrow_mut().left = true,
+            "KeyS" => this.borrow_mut().backward = true,
+            "KeyD" => this.borrow_mut().right = true,
+            _ => {}
+          }
+        }
+      }
+    );
+
+    let on_keyup : Closure< dyn Fn( _ ) > = Closure::new
+    (
+      {
+        let this = this.clone();
+        move | event : web_sys::KeyboardEvent |
+        {
+          match event.code().as_str()
+          {
+            "KeyW" => this.borrow_mut().forward = false,
+            "KeyA" => this.borrow_mut().left = false,
+            "KeyS" => this.borrow_mut().backward = false,
+            "KeyD" => this.borrow_mut().right = false,
+            _ => {}
+          }
+        }
+      }
+    );
+
+    window.set_onkeydown( Some( on_keydown.as_ref().unchecked_ref() ) );
+    window.set_onkeyup( Some( on_keyup.as_ref().unchecked_ref() ) );
+    on_keydown.forget();
+    on_keyup.forget();
+
+    this
+  }
+}
--- examples/minwebgl/raycaster/src/main.rs
+++ examples/minwebgl/raycaster/src/main.rs
@@ -1,3 +1,7 @@
+mod controls;
+
+use std::f32::consts;
+use controls::Controls;
 use minwebgl as gl;
 use gl::GL;
 
@@ -7,8 +11,10 @@ fn main()
   run();
 }
 
-const SCREEN_WIDTH : u32 = 1024;
-const SCREEN_HEIGHT : u32 = 512;
+const PI2 : f32 = consts::PI * 2.0;
+
+const WORLD_WIDTH : u32 = 1024;
+const WORLD_HEIGHT : u32 = 512;
 const MAP_SIDE_LEN : usize = 8;
 const MAP : [ u8; MAP_SIDE_LEN * MAP_SIDE_LEN ] =
 [
@@ -27,25 +33,63 @@ fn run()
   let gl = gl::context::retrieve_or_make().unwrap();
   gl.clear_color( 0.3, 0.3, 0.3, 1.0 );
 
-  let v = include_str!( "shaders/point.vert" );
-  let f = include_str!( "shaders/point.frag" );
-  let program = gl::ProgramFromSources::new( v, f ).compile_and_link( &gl ).unwrap();
-  gl.use_program( Some( &program) );
+  let point = include_str!( "shaders/point.vert" );
+  let line = include_str!( "shaders/line.vert" );
+  let fragment = include_str!( "shaders/main.frag" );
+  let point_shader = gl::ProgramFromSources::new( point, fragment ).compile_and_link( &gl ).unwrap();
+  let line_shader = gl::ProgramFromSources::new( line, fragment ).compile_and_link( &gl ).unwrap();
 
-  let loop_ = move | _t |
+  let controls = Controls::setup();
+  let mut player_pos = [ -80.0, 30.0 ];
+  let mut angle = 0.0;
+  let mut last_time = 0.0;
+
+  let loop_ = move | time |
   {
+    let time = ( time / 1000.0 ) as f32;
+    let delta_time = time - last_time;
+    last_time = time;
+
+    angle += controls.borrow().as_vec()[ 1 ] * delta_time * 2.5;
+    if angle < 0.0
+    {
+      angle = PI2 + angle % PI2;
+    }
+    if angle > PI2
+    {
+      angle %= PI2;
+    }
+    let dir = player_direction( angle );
+
+    player_pos[ 0 ] += dir[ 0 ] * delta_time * controls.borrow().as_vec()[ 0 ] * 70.0;
+    player_pos[ 1 ] += dir[ 1 ] * delta_time * controls.borrow().as_vec()[ 0 ] * 70.0;
+
+    let line_start = world2screen( &player_pos );
+    let line_end = world2screen( &[ player_pos[ 0 ] + dir[ 0 ] * 20.0, player_pos[ 1 ] + dir[ 1 ] * 20.0 ] );
+
     gl.clear( GL::COLOR_BUFFER_BIT );
 
+    gl.use_program( Some( &point_shader ) );
     draw_map( &gl );
+    draw_player( &gl, &world2screen( &player_pos ) );
 
-    gl.vertex_attrib2f( 0, 0.0, 0.0 );
-    gl.vertex_attrib1f( 1, 8.0 );
+    gl.use_program( Some( &line_shader ) );
+    gl.vertex_attrib2fv_with_f32_array( 0, &line_start );
+    gl.vertex_attrib2fv_with_f32_array( 1, &line_end );
     gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
-    gl.draw_arrays( GL::POINTS, 0, 1 );
+    gl.draw_arrays( GL::LINES, 0, 2 );
 
     true
   };
   gl::exec_loop::run( loop_ );
+}
+
+fn player_direction( angle : f32 ) -> [ f32; 2 ]
+{
+  [
+    angle.cos(),
+    angle.sin(),
+  ]
 }
 
 fn draw_map( gl : &GL )
@@ -63,7 +107,7 @@ fn draw_map( gl : &GL )
       [ 0.0, 0.0, 0.0 ]
     };
     let size = 64.0;
-    let pos = coord2screen( &[ -512.0 + size * ( col + 0.5 ), 256.0 - size * ( row + 0.5 ) ] );
+    let pos = world2screen( &[ -512.0 + size * ( col + 0.5 ), 256.0 - size * ( row + 0.5 ) ] );
     gl.vertex_attrib2fv_with_f32_array( 0, &pos );
     gl.vertex_attrib1f( 1, size - 1.0 );
     gl.vertex_attrib3fv_with_f32_array( 2, &color );
@@ -71,10 +115,18 @@ fn draw_map( gl : &GL )
   }
 }
 
-fn coord2screen( coord : &[ f32; 2 ] ) -> [ f32; 2 ]
+fn draw_player( gl : &GL, player_pos : &[ f32; 2 ] )
 {
-  const WIDTH : f32 = SCREEN_WIDTH as f32;
-  const HEIGHT : f32 = SCREEN_HEIGHT as f32;
+  gl.vertex_attrib2fv_with_f32_array( 0, player_pos );
+  gl.vertex_attrib1f( 1, 8.0 );
+  gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
+  gl.draw_arrays( GL::POINTS, 0, 1 );
+}
+
+fn world2screen( coord : &[ f32; 2 ] ) -> [ f32; 2 ]
+{
+  const WIDTH : f32 = WORLD_WIDTH as f32;
+  const HEIGHT : f32 = WORLD_HEIGHT as f32;
 
   [ coord[ 0 ] / ( WIDTH / 2.0 ), coord[ 1 ] / ( HEIGHT / 2.0 ) ]
 }
