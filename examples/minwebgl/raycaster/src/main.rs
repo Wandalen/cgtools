71beef277603a613deb5040a5eb7b5a4942748c5
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-10-30 21:14:43 +0200

    Update main.rs

--- examples/minwebgl/raycaster/src/main.rs
+++ examples/minwebgl/raycaster/src/main.rs
@@ -71,20 +71,50 @@ fn run()
     player_pos[ 0 ] += move_velocity * dir[ 0 ] * delta_time * controls.borrow().as_vec()[ 0 ];
     player_pos[ 1 ] += move_velocity * dir[ 1 ] * delta_time * controls.borrow().as_vec()[ 0 ];
 
-    let line_start = world2screen( &player_pos );
-    let line_end = world2screen( &[ player_pos[ 0 ] + dir[ 0 ] * 20.0, player_pos[ 1 ] + dir[ 1 ] * 20.0 ] );
+
+
+
+    gl.use_program( Some( &point_shader ) );
+    for ( i, item ) in MAP.iter().enumerate()
+    {
+      let col = ( i % MAP_SIDE_LEN ) as f32;
+      let row = ( i / MAP_SIDE_LEN ) as f32;
+
+      let color = if *item == 1
+      {
+        [ 0.0, 0.0, 0.0 ]
+      }
+      else
+      {
+        [ 1.0, 1.0, 1.0 ]
+      };
+
+      let posx = ( -512.0 + CELL_SIZE * ( row + 0.5 ) ) / 1024.0;
+      let posy = ( 256.0 - CELL_SIZE * ( col + 0.5 ) ) / 512.0;
+
+      gl.vertex_attrib2fv_with_f32_array( 0, &[ posx, posy ] );
+      gl.vertex_attrib1f( 1, CELL_SIZE - 1.0 );
+      gl.vertex_attrib3fv_with_f32_array( 2, &color );
+      gl.draw_arrays( GL::POINTS, 0, 1 );
+    }
+
+
+
+
+    // let line_start = world2screen( &player_pos );
+    // let line_end = world2screen( &[ player_pos[ 0 ] + dir[ 0 ] * 20.0, player_pos[ 1 ] + dir[ 1 ] * 20.0 ] );
 
     gl.clear( GL::COLOR_BUFFER_BIT );
 
-    gl.use_program( Some( &point_shader ) );
-    draw_map( &gl );
-    draw_player( &gl, &world2screen( &player_pos ) );
+    // gl.use_program( Some( &point_shader ) );
+    // draw_map( &gl );
+    // draw_player( &gl, &world2screen( &player_pos ) );
 
-    gl.use_program( Some( &line_shader ) );
-    gl.vertex_attrib2fv_with_f32_array( 0, &line_start );
-    gl.vertex_attrib2fv_with_f32_array( 1, &line_end );
-    gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
-    gl.draw_arrays( GL::LINES, 0, 2 );
+    // gl.use_program( Some( &line_shader ) );
+    // gl.vertex_attrib2fv_with_f32_array( 0, &line_start );
+    // gl.vertex_attrib2fv_with_f32_array( 1, &line_end );
+    // gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
+    // gl.draw_arrays( GL::LINES, 0, 2 );
 
     true
   };
@@ -105,6 +135,7 @@ fn draw_map( gl : &GL )
   {
     let col = ( i % MAP_SIDE_LEN ) as f32;
     let row = ( i / MAP_SIDE_LEN ) as f32;
+
     let color = if *item == 1
     {
       [ 1.0, 1.0, 1.0 ]
@@ -141,8 +172,18 @@ fn world2screen( coord : &[ f32; 2 ] ) -> [ f32; 2 ]
   [ coord[ 0 ] / ( WORLD_WIDTH / 2.0 ), coord[ 1 ] / ( WORLD_HEIGHT / 2.0 ) ]
 }
 
-fn cast_ray( pos : &[ f32; 2 ], angle : f32 )
+fn cast_rays( pos : &[ f32; 2 ], angle : f32 )
 {
+  let direction = direction( angle );
+  let step_scale_x = ( 1.0 + ( direction[ 1 ] / direction[ 0 ] ).powi( 2 ) ).sqrt();
+  let step_scale_y = ( 1.0 + ( direction[ 0 ] / direction[ 1 ] ).powi( 2 ) ).sqrt();
+  let step_x = if direction[ 0 ] < 0.0 { -1.0 } else { 1.0 };
+  let step_y = if direction[ 1 ] < 0.0 { -1.0 } else { 1.0 };
+
+  let step_x_accum = CELL_SIZE - ( pos[ 0 ] % CELL_SIZE ).abs();
+  let step_y_accum = CELL_SIZE - ( pos[ 1 ] % CELL_SIZE ).abs();
+
+
   let rays_count = 1;
   for _ in 0..rays_count
   {
