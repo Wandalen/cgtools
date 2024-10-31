f3df476cf24b297f4630aeb59a0bfc69e4d2d541
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-10-31 02:13:21 +0200

    Update main.rs

--- examples/minwebgl/raycaster/src/main.rs
+++ examples/minwebgl/raycaster/src/main.rs
@@ -17,8 +17,8 @@ const WORLD_WIDTH : f32     = 1024.0;
 const WORLD_HEIGHT : f32    = 512.0;
 const CELL_SIZE : f32       = 64.0;
 
-const MAP_SIDE_LEN : usize  = 8;
-const MAP : [ u8; MAP_SIDE_LEN * MAP_SIDE_LEN ] =
+const MAP_SIDE : usize = 8;
+const MAP : [ u8; MAP_SIDE * MAP_SIDE ] =
 [
   1, 1, 1, 1, 1, 1, 1, 1,
   1, 0, 0, 0, 0, 0, 0, 1,
@@ -44,7 +44,7 @@ fn run()
   let controls = Controls::setup();
   let rotation_velocity     = 2.5;
   let move_velocity         = 1.0;
-  let mut player_pos        = [ 2.0, 2.0 ];
+  let mut player_pos        = [ 2.5, 2.5 ];
   let mut angle             = 0.0;
   let mut last_time         = 0.0;
 
@@ -73,6 +73,9 @@ fn run()
     player_pos[ 0 ] += move_velocity * dir[ 0 ] * delta_time * controls.borrow().as_vec()[ 0 ];
     player_pos[ 1 ] += move_velocity * -dir[ 1 ] * delta_time * controls.borrow().as_vec()[ 0 ];
 
+    let end = cast_rays( &player_pos, angle );
+    // gl::info!( "{end:?}" );
+
     gl.clear( GL::COLOR_BUFFER_BIT );
 
     gl.use_program( Some( &point_shader ) );
@@ -80,8 +83,8 @@ fn run()
     // draw map
     for ( i, item ) in MAP.iter().enumerate()
     {
-      let col = ( i % MAP_SIDE_LEN ) as f32;
-      let row = ( i / MAP_SIDE_LEN ) as f32;
+      let col = ( i % MAP_SIDE ) as f32;
+      let row = ( i / MAP_SIDE ) as f32;
 
       let color = if *item == 1
       {
@@ -102,23 +105,25 @@ fn run()
     }
 
     // draw player
-    let posx = player_pos[ 0 ] / MAP_SIDE_LEN as f32 - 1.0;
-    let posy = 1.0 - player_pos[ 1 ] / MAP_SIDE_LEN as f32 * 2.0;
-    gl.vertex_attrib2fv_with_f32_array( 0, &[ posx, posy ] );
+    // transform player pos to screen space
+    let posx = player_pos[ 0 ] / MAP_SIDE as f32 - 1.0;
+    let posy = 1.0 - player_pos[ 1 ] / MAP_SIDE as f32 * 2.0;
+    let player_pos = [ posx, posy ];
+    gl.vertex_attrib2fv_with_f32_array( 0, &player_pos );
     gl.vertex_attrib1f( 1, 8.0 );
     gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
     gl.draw_arrays( GL::POINTS, 0, 1 );
 
     // draw direction line
-    let player_pos = [ posx, posy ];
     // determine start and end of line in screen space
     let line_start = player_pos;
-    let line_end = [ player_pos[ 0 ] + dir[ 0 ] * 0.2, player_pos[ 1 ] + dir[ 1 ] * 0.4 ];
+    let line_end = [ end[ 0 ] / MAP_SIDE as f32 - 1.0, 1.0 - end[ 1 ] / MAP_SIDE as f32 * 2.0 ];
     gl.use_program( Some( &line_shader ) );
     gl.vertex_attrib2fv_with_f32_array( 0, &line_start );
     gl.vertex_attrib2fv_with_f32_array( 1, &line_end );
     gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
     gl.draw_arrays( GL::LINES, 0, 2 );
+
 
     true
   };
@@ -134,64 +139,61 @@ fn direction( angle : f32 ) -> [ f32; 2 ]
   ]
 }
 
-fn draw_map( gl : &GL )
+fn cast_rays( start : &[ f32; 2 ], angle : f32 ) -> [ f32; 2 ]
 {
-  for ( i, item ) in MAP.iter().enumerate()
-  {
-    let col = ( i % MAP_SIDE_LEN ) as f32;
-    let row = ( i / MAP_SIDE_LEN ) as f32;
+  let direction = direction( angle );
+  let scale_x = ( 1.0 + ( direction[ 1 ] / direction[ 0 ] ).powi( 2 ) ).sqrt();
+  let scale_y = ( 1.0 + ( direction[ 0 ] / direction[ 1 ] ).powi( 2 ) ).sqrt();
+  let mut accum_x = 0.0;
+  let mut accum_y = 0.0;
 
-    let color = if *item == 1
+  let step_x = if direction[ 0 ] < 0.0
+  {
+    accum_x += start[ 0 ].fract() * scale_x;
+    -1.0
+  }
+  else
+  {
+    accum_x += ( 1.0 - start[ 0 ].fract() ) * scale_x;
+    1.0
+  };
+  let step_y = if direction[ 1 ] < 0.0
+  {
+    accum_y += ( 1.0 - start[ 1 ].fract() ) * scale_y;
+    1.0
+  }
+  else
+  {
+    accum_y += start[ 1 ].fract() * scale_y;
+    -1.0
+  };
+
+  for _ in 0..24
+  {
+    if accum_x < accum_y
     {
-      [ 1.0, 1.0, 1.0 ]
+      let pos = [ start[ 0 ] + direction[ 0 ] * accum_x, start[ 1 ] + -direction[ 1 ] * accum_x ];
+      let col = pos[ 0 ].trunc() as usize;
+      let row = pos[ 1 ].trunc() as usize;
+      let id = row * MAP_SIDE + col;
+      if MAP[ id ] == 0
+      {
+        break;
+      }
     }
     else
     {
-      [ 0.0, 0.0, 0.0 ]
-    };
-
-    let pos = world2screen
-    (
-      &[
-        -WORLD_WIDTH / 2.0 + CELL_SIZE * ( col + 0.5 ),
-        WORLD_HEIGHT / 2.0 - CELL_SIZE * ( row + 0.5 )
-      ]
-    );
-    gl.vertex_attrib2fv_with_f32_array( 0, &pos );
-    gl.vertex_attrib1f( 1, CELL_SIZE - 1.0 );
-    gl.vertex_attrib3fv_with_f32_array( 2, &color );
-    gl.draw_arrays( GL::POINTS, 0, 1 );
+      let pos = [ start[ 0 ] + direction[ 0 ] * accum_y, start[ 1 ] + -direction[ 1 ] * accum_y ];
+      let col = pos[ 0 ].trunc() as usize;
+      let row = pos[ 1 ].trunc() as usize;
+      let id = row * MAP_SIDE + col;
+    }
   }
-}
+  todo!()
+  // end
+  // let rays_count = 1;
+  // for _ in 0..rays_count
+  // {
 
-fn draw_player( gl : &GL, player_pos : &[ f32; 2 ] )
-{
-  gl.vertex_attrib2fv_with_f32_array( 0, player_pos );
-  gl.vertex_attrib1f( 1, 8.0 );
-  gl.vertex_attrib3f( 2, 1.0, 0.5, 0.0 );
-  gl.draw_arrays( GL::POINTS, 0, 1 );
-}
-
-fn world2screen( coord : &[ f32; 2 ] ) -> [ f32; 2 ]
-{
-  [ coord[ 0 ] / ( WORLD_WIDTH / 2.0 ), coord[ 1 ] / ( WORLD_HEIGHT / 2.0 ) ]
-}
-
-fn cast_rays( pos : &[ f32; 2 ], angle : f32 )
-{
-  let direction = direction( angle );
-  let step_scale_x = ( 1.0 + ( direction[ 1 ] / direction[ 0 ] ).powi( 2 ) ).sqrt();
-  let step_scale_y = ( 1.0 + ( direction[ 0 ] / direction[ 1 ] ).powi( 2 ) ).sqrt();
-  let step_x = if direction[ 0 ] < 0.0 { -1.0 } else { 1.0 };
-  let step_y = if direction[ 1 ] < 0.0 { -1.0 } else { 1.0 };
-
-  let step_x_accum = CELL_SIZE - ( pos[ 0 ] % CELL_SIZE ).abs();
-  let step_y_accum = CELL_SIZE - ( pos[ 1 ] % CELL_SIZE ).abs();
-
-
-  let rays_count = 1;
-  for _ in 0..rays_count
-  {
-
-  }
+  // }
 }
