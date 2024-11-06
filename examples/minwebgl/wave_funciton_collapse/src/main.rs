61ebf5aa855e0baa930401a125b501c79c7d94e4
Bilan Vadim <vadimb892@gmail.com>
2024-11-06 14:17:21 +0200

    Added wave function collapse example

---
+++ examples/minwebgl/wave_funciton_collapse/src/main.rs
@@ -0,0 +1,15 @@
+//! Just draw a large point in the middle of the screen.
+
+use minwebgl as gl;
+use gl::{ GL };
+
+fn run() -> Result< (), gl::WebglError >
+{
+  
+  Ok(())
+}
+
+fn main()
+{
+  run().unwrap()
+}
