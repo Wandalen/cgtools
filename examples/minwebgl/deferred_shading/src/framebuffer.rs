67fa9a013fd92254ad39cbaac89d06ddb2c1b650
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-11-07 19:54:05 +0200

    Update framebuffer.rs

--- examples/minwebgl/deferred_shading/src/framebuffer.rs
+++ examples/minwebgl/deferred_shading/src/framebuffer.rs
@@ -8,7 +8,11 @@ use web_sys::
   wasm_bindgen::prelude::*,
   js_sys,
 };
-use std::collections::HashMap;
+use std::ops::Index;
+
+// Maximum amount of ColorAttachments supported by WebGl2
+// Actual amount may be reduced by hardware implementation
+const MAX_COLOR_ATTACHMENTS : usize = 16;
 
 #[ repr( u32 ) ]
 #[ derive( Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord ) ]
@@ -71,73 +75,66 @@ impl Attachment
 pub struct Framebuffer
 {
   framebuffer : WebGlFramebuffer,
-  textures : [ Option< WebGlTexture >; 16 ],
-  renderbuffers : [ Option< WebGlRenderbuffer >; 16 ],
-  attachments : HashMap< ColorAttachment, Attachment >,
-  depthbuffer : Option< ( DepthAttachment, Attachment ) >,
+  attachments : [ Option< Attachment >; MAX_COLOR_ATTACHMENTS ],
+  depth_attachment : Option< ( DepthAttachment, Attachment ) >,
 }
 
 impl Framebuffer
 {
-  pub fn t_attachment( &self, index : ColorAttachment ) -> Option< &WebGlTexture >
-  {
-    let index = index as usize - ColorAttachment::N0 as usize;
-    self.textures[ index ].as_ref()
-  }
-
-  pub fn r_attachment( &self, index : ColorAttachment ) -> Option< &WebGlRenderbuffer >
-  {
-    let index = index as usize - ColorAttachment::N0 as usize;
-    self.renderbuffers[ index ].as_ref()
-  }
-
   pub fn attachment( &self, index : ColorAttachment ) -> Option< &Attachment >
   {
-    self.attachments.get( &index )
+    let index = index as usize - ColorAttachment::N0 as usize;
+    match self.attachments.get( index ).as_ref()
+    {
+      Some( attachment ) => attachment.as_ref(),
+      None => None,
+    }
   }
 
-  pub fn depthbuffer( &self ) -> Option< &Attachment >
+  pub fn depth_attachment( &self ) -> Option< &Attachment >
   {
-    self.depthbuffer.as_ref().map( | ( _, a ) | a )
+    self.depth_attachment.as_ref().map( | ( _, a ) | a )
   }
 
-  /// binds all of attachments for drawing
+  /// Binds all of color attachments for drawing
   pub fn bind_draw( &self, gl : &GL )
   {
     // forms an array of attachments in their respective positions
     // for example if framebuffer has attachment0 and attachment2
     // then the array would be [ GL::ColorAttachment0, GL::NONE, GL::ColorAttachment2 ]
     // this is how WebGl wants it to be
-    let mut attachments = self.attachments.keys().map( | a | *a as u32 ).collect::< Box< _ > >();
-    attachments.sort();
 
-    let mut drawbuffers = vec![];
-    let mut start = GL::COLOR_ATTACHMENT0;
-    for attachment in attachments
-    {
-      drawbuffers.extend( ( start..attachment ).map( | _ | GL::NONE ) );
-      drawbuffers.push( attachment );
-      start = attachment + 1;
-    }
-    gl::info!( "{drawbuffers:?}" );
-    let iter = drawbuffers.into_iter().map( | v | JsValue::from_f64( v as f64 ) );
+    // find index of last non-None value
+    let last = self.attachments.iter().rposition( | v | v.is_some() ).unwrap_or( 0 );
 
-    let iter = self.textures.iter()
-    .zip( self.renderbuffers.iter() )
+    let iter = self.attachments[ 0..=last ]
+    .iter()
     .enumerate()
-    .map( | ( i, ( t, r ) ) | if t.is_some() || r.is_some() { GL::COLOR_ATTACHMENT0 + i as u32 } else { GL::NONE } )
+    .map
+    (
+      | ( i, a ) |
+      if a.is_some()
+      {
+        GL::COLOR_ATTACHMENT0 + i as u32
+      }
+      else
+      {
+        GL::NONE
+      }
+    )
     .map( | v | JsValue::from_f64( v as f64 ) );
 
     gl.bind_framebuffer( GL::DRAW_FRAMEBUFFER, Some( &self.framebuffer ) );
-    gl.draw_buffers( &js_sys::Array::from_iter( iter ).into() );
+    gl.draw_buffers( &js_sys::Array::from_iter( iter ) );
   }
 
-  /// binds specific attachments for drawing
+  /// Binds specific color attachment for drawing
   pub fn bind_draw_nth( &self, index : ColorAttachment, gl : &GL ) -> Result< (), String >
   {
-    if !self.attachments.contains_key( &index )
+    let index = index as usize - ColorAttachment::N0 as usize;
+    if !self.attachments[ index ].is_none()
     {
-      return Err( format!( "Framebuffer does not has {:?}", index as u32 ) );
+      return Err( format!( "Framebuffer does not has ColorAttachment{:?}", index as u32 ) );
     }
 
     let iter = ( 0..( index as u32 ) )
@@ -150,12 +147,13 @@ impl Framebuffer
     Ok( () )
   }
 
-  /// binds specific attachment for reading
+  /// Binds specific color attachment for reading
   pub fn bind_read( &self, index : ColorAttachment, gl : &GL ) -> Result< (), String >
   {
-    if !self.attachments.contains_key( &index )
+    let index = index as usize - ColorAttachment::N0 as usize;
+    if !self.attachments[ index ].is_none()
     {
-      return Err( format!( "Framebuffer does not has {:?}", index as u32 ) );
+      return Err( format!( "Framebuffer does not has ColorAttachment{:?}", index as u32 ) );
     }
 
     gl.bind_framebuffer( GL::READ_FRAMEBUFFER, Some( &self.framebuffer ) );
@@ -163,22 +161,34 @@ impl Framebuffer
     Ok( () )
   }
 
-  /// binds depth buffer for reading
-  pub fn bind_read_depth( &self, gl : &GL ) -> Result< (), String >
+  /// Binds depth attachment for reading
+  pub fn bind_read_depth_attachment( &self, gl : &GL ) -> Result< (), String >
   {
-    if self.depthbuffer.is_none()
+    if self.depth_attachment.is_none()
     {
       return Err( format!( "Framebuffer does not has depthbuffer" ) );
     }
+
     gl.bind_framebuffer( GL::READ_FRAMEBUFFER, Some( &self.framebuffer ) );
-    gl.read_buffer( self.depthbuffer.as_ref().unwrap().0 as u32 );
+    gl.read_buffer( self.depth_attachment.as_ref().unwrap().0 as u32 );
     Ok( () )
   }
 }
 
+impl Index< ColorAttachment > for Framebuffer
+{
+    type Output = Attachment;
+
+    fn index( &self, index: ColorAttachment ) -> &Self::Output
+    {
+      let index = index as usize - ColorAttachment::N0 as usize;
+      self.attachments[ index ].as_ref().unwrap()
+    }
+}
+
 pub struct FramebufferBuilder
 {
-  attachments : HashMap< ColorAttachment, Attachment >,
+  attachments : [ Option< Attachment >; MAX_COLOR_ATTACHMENTS ],
   depthbuffer : Option< ( DepthAttachment, Attachment ) >,
 }
 
@@ -195,7 +205,8 @@ impl FramebufferBuilder
 
   pub fn attachment( mut self, index : ColorAttachment, attachment : Attachment ) -> Self
   {
-    _ = self.attachments.insert( index, attachment );
+    let index = index as usize - ColorAttachment::N0 as usize;
+    self.attachments[ index ] = Some( attachment );
     self
   }
 
@@ -214,44 +225,41 @@ impl FramebufferBuilder
     };
     gl.bind_framebuffer( GL::FRAMEBUFFER, Some( &framebuffer ) );
 
-    let mut textures = [ const { None }; 16 ];
-    let mut renderbuffers = [ const { None }; 16 ];
-
-    for ( index, attachment ) in self.attachments.iter()
+    for ( i, attachment ) in self.attachments.iter().enumerate()
     {
-      match attachment
+      let index = GL::COLOR_ATTACHMENT0 + i as u32;
+      if let Some( attachment ) = attachment
       {
-        Attachment::Texture( texture ) =>
+        match attachment
         {
-          gl.framebuffer_texture_2d
-          (
-            GL::FRAMEBUFFER,
-            *index as u32,
-            GL::TEXTURE_2D,
-            Some( texture ),
-            0
-          );
-          let index = *index as usize - ColorAttachment::N0 as usize;
-          textures[ index ] = Some( texture.clone() );
-        }
-        Attachment::Renderbuffer( renderbuffer ) =>
-        {
-          gl.framebuffer_renderbuffer
-          (
-            GL::FRAMEBUFFER,
-            *index as u32,
-            GL::RENDERBUFFER,
-            Some( renderbuffer ),
-          );
-          let index = *index as usize - ColorAttachment::N0 as usize;
-          renderbuffers[ index ] = Some( renderbuffer.clone() );
+          Attachment::Texture( texture ) =>
+          {
+            gl.framebuffer_texture_2d
+            (
+              GL::FRAMEBUFFER,
+              index,
+              GL::TEXTURE_2D,
+              Some( texture ),
+              0
+            );
+          }
+          Attachment::Renderbuffer( renderbuffer ) =>
+          {
+            gl.framebuffer_renderbuffer
+            (
+              GL::FRAMEBUFFER,
+              index,
+              GL::RENDERBUFFER,
+              Some( renderbuffer ),
+            );
+          }
         }
       }
     }
 
     if let Some( ( r#type, attachment ) ) = &self.depthbuffer
     {
-      match attachment
+      match &attachment
       {
         Attachment::Texture( texture ) =>
         {
@@ -260,7 +268,7 @@ impl FramebufferBuilder
             GL::FRAMEBUFFER,
             *r#type as u32,
             GL::TEXTURE_2D,
-            Some( texture ),
+            Some( &texture ),
             0
           );
         }
@@ -271,7 +279,7 @@ impl FramebufferBuilder
             GL::FRAMEBUFFER,
             *r#type as u32,
             GL::RENDERBUFFER,
-            Some( renderbuffer ),
+            Some( &renderbuffer ),
           );
         }
       }
@@ -289,11 +297,9 @@ impl FramebufferBuilder
     (
       Framebuffer
       {
-        textures,
-        renderbuffers,
         framebuffer,
-        attachments: self.attachments,
-        depthbuffer: self.depthbuffer,
+        attachments : self.attachments,
+        depth_attachment: self.depthbuffer,
       }
     )
   }
