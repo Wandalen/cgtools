926c26ad2a03c6407dff53c36b05df3a6baffac1
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2024-11-07 16:37:19 +0200

    added doc

--- module/min/mingl/src/web/dom/image.rs
+++ module/min/mingl/src/web/dom/image.rs
@@ -5,19 +5,25 @@ mod private
   use web_sys::HtmlImageElement;
   use wasm_bindgen::prelude::*;
 
+  /// Creates new `HtmlImageElement` and waits asynchronously until it is loaded.
+  /// Unfortunately there's no error message provided by JS API on error occurrence
+  /// so in this case returns just `None`.
+  /// Assume that `src` will be adjusted to "origin/static/`src`".
   pub async fn load( src : &str ) -> Option< HtmlImageElement >
   {
     let image = HtmlImageElement::new().ok()?;
-    image.set_src( src );
 
+    // here's js promises are used to get
+    // either successful or unsuccessful result of image loading.
+    // promise then converted into future and awaited until it's done
     let promise = js_sys::Promise::new
     (
       &mut | resolve, reject |
       {
-        let on_load = Closure::once( move || resolve.call1( &JsValue::NULL, &JsValue::NULL ).unwrap() );
+        let on_load = Closure::< dyn Fn() >::new( move || _ = resolve.call1( &JsValue::NULL, &JsValue::NULL ).unwrap() );
         image.set_onload( Some( on_load.as_ref().unchecked_ref() ) );
 
-        let on_error = Closure::once( move || reject.call1( &JsValue::NULL, &JsValue::NULL ).unwrap() );
+        let on_error = Closure::< dyn Fn() >::new( move || _ = reject.call1( &JsValue::NULL, &JsValue::NULL ).unwrap() );
         image.set_onerror( Some( on_error.as_ref().unchecked_ref() ) );
 
         on_load.forget();
@@ -25,16 +31,26 @@ mod private
       }
     );
 
-    if let Err( _ ) = JsFuture::from( promise ).await
-    {
-      image.remove();
-      return None;
-    }
+    let window = web_sys::window().expect( "Should have a window" );
+    let origin = window.location().origin().expect( "Should have an origin" );
+    let src = format!( "{origin}/static/{src}" );
+    image.set_src( &src );
 
-    Some( image )
+    let res = JsFuture::from( promise ).await;
+    image.set_onload( None );
+    image.set_onerror( None );
+
+    match res
+    {
+      Ok( _ ) => Some( image ),
+      Err( _ ) =>
+      {
+        image.remove();
+        None
+      }
+    }
   }
 }
-
 
 crate::mod_interface!
 {
