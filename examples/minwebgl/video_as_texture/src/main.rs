872edcb35615bbe1b625f21672dd57c226a1bd1a
projdysvit <perekotypole@proton.me>
2024-11-08 14:33:46 +0200

    implemented load_media in mingl/web

--- examples/minwebgl/video_as_texture/src/main.rs
+++ examples/minwebgl/video_as_texture/src/main.rs
@@ -1,4 +1,4 @@
-use minwebgl::{self as gl, JsCast};
+use minwebgl::{self as gl, web_sys, JsCast};
 
 fn main()
 {
@@ -19,7 +19,20 @@ async fn run() -> Result< (), gl::WebglError >
   let video_width = 640;
   let video_height = 480;
 
-  let video_element = load_video( path, video_width, video_height )
+  let video_element = gl::file::load_media
+  (
+    "simon's_cat.mp4",
+    | doc |
+    {
+      let video_element = doc.create_element( "video" )?
+      .dyn_into::< web_sys::HtmlVideoElement >()?;
+      video_element.set_width( video_width );
+      video_element.set_height( video_height );
+      video_element.set_muted( true );
+      video_element.set_loop( true );
+      Ok( video_element )
+    }
+  )
   .await
   .expect( "Failed to load video" );
   let texture = gl.create_texture().expect( "Failed to create texture" );
@@ -43,44 +56,4 @@ async fn run() -> Result< (), gl::WebglError >
   gl::exec_loop::run( update_and_draw );
 
   Ok( () )
-}
-
-async fn load_video( path : &str, video_width : u32, video_height : u32 ) -> Result< gl::web_sys::HtmlVideoElement, gl::wasm_bindgen::JsValue >
-{
-  let document = gl::web_sys::window().unwrap().document().unwrap();
-  let video_element = document
-  .create_element( "video" )?
-  .dyn_into::< gl::web_sys::HtmlVideoElement >()?;
-
-  // Creates a new Promise instance that will execute when the video is ready to play.
-  let load_promise = gl::js_sys::Promise::new
-  (
-    &mut | resolve, _ |
-    {
-      // Create a Closure that will be called when the video is ready to play.
-      let oncanplay = gl::wasm_bindgen::prelude::Closure::once_into_js
-      (
-        move ||
-        {
-          // When the oncanplay event is triggered, call the resolve function to complete the Promise.
-          resolve.call0( &gl::wasm_bindgen::JsValue::NULL ).unwrap();
-        }
-      );
-
-      // The oncanplay callback calls resolve to complete the Promise.
-      video_element.set_oncanplay( Some( oncanplay.as_ref().unchecked_ref() ) );
-    }
-  );
-
-  video_element.set_width( video_width );
-  video_element.set_height( video_height );
-  video_element.set_loop( true );
-  video_element.set_muted( true );
-  video_element.set_src( path );
-  let _ = video_element.play()?;
-
-  // Waits for the Promise created earlier to complete when the video is ready to play.
-  gl::JsFuture::from( load_promise ).await?;
-
-  Ok( video_element )
 }
--- module/min/mingl/src/web/file.rs
+++ module/min/mingl/src/web/file.rs
@@ -32,11 +32,68 @@ mod private
     Ok( data )
   }
 
+  pub async fn load_media< T, F >( file_name : &str, init_element : F ) -> Result< T, JsValue >
+  where
+    T : JsCast + AsRef< web_sys::HtmlElement >,
+    F : FnOnce( &web_sys::Document ) -> Result< T, JsValue >,
+  {
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let origin = window.location().origin().unwrap();
+    let url = format!( "{}/static/{}", origin, file_name );
+
+    let element = init_element( &document )?;
+
+    let load_promise = js_sys::Promise::new
+    (
+      &mut | resolve, reject |
+      {
+        let on_event = wasm_bindgen::prelude::Closure::once_into_js
+        (
+          move ||
+          {
+            resolve.call0( &JsValue::NULL ).unwrap();
+          }
+        );
+
+        let on_error = wasm_bindgen::prelude::Closure::once_into_js
+        (
+          move ||
+          {
+            reject.call1( &JsValue::NULL, &JsValue::from( "Failed to load media" ) ).unwrap();
+          }
+        );
+
+        if let Some( image_element ) = element.dyn_ref::< web_sys::HtmlImageElement >()
+        {
+          image_element.set_src( &url );
+
+          image_element.set_onload( Some( on_event.as_ref().unchecked_ref() ) );
+          image_element.set_onerror( Some( on_error.as_ref().unchecked_ref() ) );
+        }
+
+        if let Some( video_element ) = element.dyn_ref::< web_sys::HtmlVideoElement >()
+        {
+          video_element.set_src( &url );
+          let _ = video_element.play().unwrap();
+
+          video_element.set_oncanplay( Some( on_event.as_ref().unchecked_ref() ) );
+          video_element.set_onerror( Some( on_error.as_ref().unchecked_ref() ) );
+        }
+      }
+    );
+
+    JsFuture::from( load_promise ).await?;
+
+    Ok( element )
+  }
+
 }
 
 crate::mod_interface!
 {
 
   own use load;
+  own use load_media;
 
 }
