06c76383a0bc2b55f74bf70d4ab0ce49a2af2c55
projdysvit <perekotypole@proton.me>
2024-11-08 14:43:40 +0200

    implemented load_media in mingl/web

--- examples/minwebgl/sprite_animation/src/main.rs
+++ examples/minwebgl/sprite_animation/src/main.rs
@@ -17,7 +17,7 @@ async fn run() -> Result< (), gl::WebglError >
   gl.use_program( Some( &program ) );
 
   // Settings for `rock.png` sprite sheets
-  let path = "static/rock.png";
+  let file_name = "rock.png";
   let sprties_in_row = 8;
   let sprites_in_col = 8;
   let sprite_width = 128;
@@ -25,7 +25,19 @@ async fn run() -> Result< (), gl::WebglError >
   let amount = 64;
   let frame_rate = 24.0;
 
-  let img_element = load_img( path ).await.expect( "Failed to load image" );
+  let img_element = gl::file::load_media
+  (
+    file_name,
+    | doc |
+    {
+      let img_element = doc.create_element( "img" )?
+      .dyn_into::< web_sys::HtmlImageElement >()?;
+
+      Ok( img_element )
+    }
+  )
+  .await
+  .expect( "Failed to load image" );
   gl::texture::d2::upload_sprite( &gl, &img_element, sprties_in_row, sprites_in_col, sprite_width, sprite_height, amount )?;
 
   let update_and_draw =
@@ -49,39 +61,4 @@ async fn run() -> Result< (), gl::WebglError >
   gl::exec_loop::run( update_and_draw );
 
   Ok( () )
-}
-
-async fn load_img( path : &str ) -> Result< web_sys::HtmlImageElement, gl::wasm_bindgen::JsValue >
-{
-  let document = gl::web_sys::window().unwrap().document().unwrap();
-  let img_element = document.create_element( "img" )?
-  .dyn_into::< gl::web_sys::HtmlImageElement >()?;
-  img_element.style().set_property( "display", "none" )?;
-
-  // Creates a new Promise instance that will execute when the image is loaded.
-  let load_promise = gl::js_sys::Promise::new
-  (
-    &mut | resolve, _ |
-    {
-      // Create a Closure that will be called when the image is loaded.
-      let onload = gl::wasm_bindgen::closure::Closure::once_into_js
-      (
-        move ||
-        {
-          // When the onload event is triggered, call the resolve function to complete the Promise.
-          resolve.call0( &gl::JsValue::NULL ).unwrap();
-        }
-      );
-
-      // The onload callback calls resolve to complete the Promise.
-      img_element.set_onload( Some( onload.as_ref().unchecked_ref() ) );
-    }
-  );
-
-  img_element.set_src( path );
-
-  // Waits for the Promise created earlier to complete when the image is loaded.
-  gl::JsFuture::from( load_promise ).await?;
-
-  Ok( img_element )
 }
