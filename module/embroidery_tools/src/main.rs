ae38884cc28c4dcdf96a544fb9cb100227639707
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2025-03-03 22:29:41 +0200

    upd

--- module/embroidery_tools/src/main.rs
+++ module/embroidery_tools/src/main.rs
@@ -1,17 +1,16 @@
 use embroidery_tools::thread::{Color, Thread};
 
 fn main() {
-    let bytes = std::fs::read(r"D:\cgtools\module\embroidery_tools\without.pes").unwrap();
+    let bytes = std::fs::read("without.pes").unwrap();
     let mut emb = embroidery_tools::format::pes::read_bytes(&bytes, "without.pes").unwrap();
-    // emb.threads[0] = Thread {
-    //     color: Color {
-    //         r: 34,
-    //         g: 29,
-    //         b: 228,
-    //     },
-    //     ..Default::default()
-    // };
-    // println!("{:?}", &emb.stitches[..10]);
-    embroidery_tools::format::pes::write_path(&emb, "path.pes").unwrap();
-    // println!("{:?}", emb.threads);
+    emb.threads[0] = Thread {
+        color: Color {
+            r: 34,
+            g: 29,
+            b: 228,
+        },
+        ..Default::default()
+    };
+
+    embroidery_tools::format::pes::write_path(&emb, "test.pes").unwrap();
 }
