c2de86c9f4ff75c7002b3fdf995c68ebeaa4c72c
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2025-03-19 02:14:18 +0200

    upd

---
+++ examples/minwebgl/hexagonal_grid/src/main.rs
@@ -0,0 +1,66 @@
+use std::marker::PhantomData;
+use minwebgl as gl;
+
+fn main()
+{
+  gl::browser::setup( Default::default() );
+}
+
+pub struct PointyTop;
+
+pub struct FlatTop;
+
+pub struct Hex< T >
+{
+  r#type: PhantomData< T >,
+  size: f32,
+}
+
+impl< T > Hex< T >
+{
+  pub fn new( size: f32 ) -> Self
+  {
+    Self { r#type : PhantomData, size }
+  }
+
+  pub fn size( &self ) -> f32
+  {
+    self.size
+  }
+}
+
+impl Hex< PointyTop >
+{
+  pub fn width( &self ) -> f32
+  {
+    3.0f32.sqrt() * self.size
+  }
+
+  pub fn height( &self ) -> f32
+  {
+    2.0 * self.size
+  }
+}
+
+impl Hex< FlatTop >
+{
+  pub fn width( &self ) -> f32
+  {
+    2.0 * self.size
+  }
+
+  pub fn height( &self ) -> f32
+  {
+    3.0f32.sqrt() * self.size
+  }
+}
+
+struct Coordinate
+{
+
+}
+
+struct HexGrid
+{
+
+}
