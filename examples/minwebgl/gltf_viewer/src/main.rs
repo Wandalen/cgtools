56f7791b66911bc0d544e33754873fd68d577b96
JekiXD <ambrejine00@gmail.com>
2025-03-31 17:38:01 +0300

    Added texture loading

--- examples/minwebgl/gltf_viewer/src/main.rs
+++ examples/minwebgl/gltf_viewer/src/main.rs
@@ -5,23 +5,30 @@ use std::
   cell::RefCell, collections::{ HashMap, HashSet }, rc::Rc 
 };
 
+use gltf::Gltf;
+use material::Material;
 // use material::{ GLMaterial, TextureType };
 // use mesh::GLMesh;
 use mingl::CameraOrbitControls;
 use minwebgl::{ self as gl, JsCast };
+use texture::Texture;
 use web_sys::wasm_bindgen::prelude::Closure;
 
 // mod mesh;
 mod camera_controls;
-// mod material;
-mod scene;
-mod node;
+mod material;
+//mod scene;
+//mod node;
+mod texture;
+mod sampler;
 
 async fn run() -> Result< (), gl::WebglError >
 {
   gl::browser::setup( Default::default() );
   let canvas = gl::canvas::make()?;
   let gl = gl::context::from_canvas( &canvas )?;
+  let window = gl::web_sys::window().unwrap();
+  let document = window.document().unwrap();
 
   let width = canvas.width() as f32;
   let height = canvas.height() as f32;
@@ -60,6 +67,127 @@ async fn run() -> Result< (), gl::WebglError >
   let camera = Rc::new( RefCell::new( camera ) );
 
   camera_controls::setup_controls( &canvas, &camera );
+
+  let gltf_file_path = "dodge-challenger/gltf";
+
+  let gltf_slice= gl::file::load( &format!( "{}/scene.gltf", gltf_file_path ) ).await.expect( "Failed to load gltf file" );
+  let mut gltf_file = Gltf::from_slice( &gltf_slice ).unwrap();
+
+  let mut buffers : Vec< gl::js_sys::Uint8Array > = Vec::new();
+
+  // Move the GLB bin into buffers
+  if let Some( blob ) = gltf_file.blob.as_mut()
+  {
+    let blob = std::mem::take( blob );
+    buffers.push( blob.as_slice().into() );
+  }
+
+  for gltf_buffer in gltf_file.buffers()
+  {
+    match gltf_buffer.source()
+    {
+      gltf::buffer::Source::Uri( uri ) =>
+      {
+        let buffer = gl::file::load( &format!( "{}/{}", gltf_file_path, uri ) ).await
+        .expect( "Failed to load a buffer" );
+
+        buffers.push( buffer.as_slice().into() );
+      },
+      _ => {}
+    }
+  }
+
+  gl::info!( "Bufffers: {}", buffers.len() );
+
+  // Upload images 
+  let images = Rc::new( RefCell::new( Vec::new() ) );
+
+  // Creates an <img> html elements, and sets its src property to 'src' parameter
+  // When the image is loaded, createa a texture and adds it to the 'images' array
+  let upload_texture = | src : Rc< String > | {
+    let img_element = document.create_element( "img" ).unwrap().dyn_into::< gl::web_sys::HtmlImageElement >().unwrap();
+    img_element.style().set_property( "display", "none" ).unwrap();
+    let load_texture : Closure< dyn Fn() > = Closure::new
+    ( 
+      {
+        let images = images.clone();
+        let gl = gl.clone();
+        let img = img_element.clone();
+        let src = src.clone();
+        move || 
+        {
+          let texture = gl::texture::d2::upload( &gl, &img );
+
+          if texture.is_some()
+          {
+            images.borrow_mut().push( texture.unwrap() );
+          }
+
+          match gl::web_sys::Url::revoke_object_url( &src )
+          {
+            Ok( _ ) => { gl::info!( "Remove object url: {}", &src ) },
+            Err( _ ) => { gl::info!( "Not an object url: {}", &src ) }
+          }
+
+          img.remove();
+        }
+      }
+    );
+
+    img_element.set_onload( Some( load_texture.as_ref().unchecked_ref() ) );
+    img_element.set_src( &src );
+    load_texture.forget();
+  };
+
+  // If a source of an image is Uri - load the file
+  // If a source of an image is View - create a blob from buffer, then turn it into an Object Url,
+  // then load an image from the url
+  for gltf_image in gltf_file.images()
+  {
+    match  gltf_image.source() 
+    {
+      gltf::image::Source::Uri { uri, mime_type: _ } => 
+      {
+        upload_texture( Rc::new( format!( "{}/{}", gltf_file_path, uri ) ) );
+      },
+      gltf::image::Source::View { view, mime_type } => 
+      {
+        let buffer = buffers[ view.buffer().index() ].clone();
+        let blob = {
+          let options = gl::web_sys::BlobPropertyBag::new();
+          options.set_type( mime_type );
+
+          let mut blob_parts = Vec::new();
+          blob_parts.push( buffer );
+
+          gl::web_sys::Blob::new_with_u8_array_sequence_and_options( &( blob_parts.into() ), &options )
+        }.expect( "Failed to create a Blob" );
+
+        let url = gl::web_sys::Url::create_object_url_with_blob( &blob ).expect( "Failed to create object url" );  
+        upload_texture( Rc::new( url ) );
+      }
+    }
+  }
+
+  gl::info!( "Images: {}", images.borrow().len() );
+
+  // Create textures
+  let mut textures = Vec::new();
+  for gltf_t in gltf_file.textures()
+  {
+    let t = Texture::new( &images.borrow(), gltf_t );
+    textures.push( t );
+  }
+
+  // Create materials
+  let mut materials = Vec::new();
+  for gltf_m in gltf_file.materials()
+  {
+    let m = Material::new( gltf_m, &textures );
+    materials.push( m );
+  }
+
+  gl::log::info!( "{:?}", gltf_file.materials().len() );
 
   gl.enable( gl::DEPTH_TEST );
   gl.enable( gl::BLEND );
