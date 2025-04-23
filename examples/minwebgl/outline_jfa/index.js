02265b142e6bfe4b4ab33b684c3f2cac62d65d4d
Bilan Vadim <vadimb892@gmail.com>
2025-04-23 17:14:30 +0300

    feat: add input handling

--- examples/minwebgl/outline_jfa/index.js
+++ examples/minwebgl/outline_jfa/index.js
@@ -1,5 +1,7 @@
 import init, { App } from './pkg/outline_jfa.js';
 
+const state = new InputState();
+
 async function run() {
     await init();
 
@@ -11,16 +13,46 @@ async function run() {
         const app = new App('canvas', canvas.width, canvas.height);
 
         function renderLoop(timestamp) {
-            app.tick(timestamp);
+            state.set_timestamp(timestamp);
+            app.tick(state);
+            state.reset();
             requestAnimationFrame(renderLoop);
         }
 
         requestAnimationFrame(renderLoop);
 
-        window.addEventListener('resize', () => {
+        window.addEventListener("resize", () => {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
             app.resize(canvas.width, canvas.height);
+        });
+
+        window.addEventListener(
+            "keydown",
+            (event) => {
+                state.add_keyboard_event(event);
+            },
+            false,
+        );
+        
+        window.addEventListener(
+            "keyup", 
+            (event) => {
+                state.add_keyboard_event(event);
+            },
+            false,
+        );
+
+        myPics.addEventListener("mousedown", (event) => {
+            state.add_mouse_event(event);
+        });
+          
+        myPics.addEventListener("mousemove", (event) => {
+            state.add_mouse_event(event);
+        });
+        
+        window.addEventListener("mouseup", (event) => {
+            state.add_mouse_event(event);
         });
 
     } catch (error) {
---
+++ examples/minwebgl/outline_jfa/src/input.rs
@@ -0,0 +1,52 @@
+use js_sys::wasm_bindgen::{JsCast, JsValue};
+use web_sys::{KeyboardEvent, MouseEvent};
+
+#[wasm_bindgen]
+#[derive(Default)]
+pub struct InputState {
+    timestamp: Option<f64>,
+    viewport: Option<(u32, u32)>,
+    keyboard_events: Vec<KeyboardEvent>,
+    mouse_event: Vec<MouseEvent>
+}
+
+#[wasm_bindgen]
+impl InputState{
+    #[wasm_bindgen(constructor)]
+    fn new() -> Self{
+        Self::default()
+    }
+
+    #[wasm_bindgen]
+    fn reset(&mut self){
+        *self = Self{
+            ..Default::default()
+        };
+    }
+
+    #[wasm_bindgen]
+    fn set_timestamp(&mut self, timestamp: f64){
+        self.timestamp = Some(timestamp);
+    }
+
+    #[wasm_bindgen]
+    fn set_viewport(&mut self, width: u32, height: u32){
+        self.viewport = Some((width, height));
+    }
+
+    #[wasm_bindgen]
+    fn add_keyboard_event(&mut self, keyboard_event: JsValue){
+        let Ok(keyboard_event) = keyboard_event.dyn_into::<KeyboardEvent>() else{
+            return;
+        };
+        self.keyboard_events.push(keyboard_event);
+    }
+
+    #[wasm_bindgen]
+    fn add_mouse_event(&mut self, mouse_event: JsValue){
+        let Ok(mouse_event) = keyboard_event.dyn_into::<MouseEvent>() else{
+            return;
+        };
+        self.mouse_events.push(mouse_event);
+    }
+}
