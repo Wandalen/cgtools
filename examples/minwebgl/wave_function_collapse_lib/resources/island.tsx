307f415cf94e64ae931f8a7a4a04a0d4293147cb
Bilan Vadim <vadimb892@gmail.com>
2025-04-28 15:01:20 +0300

    update assets

--- examples/minwebgl/wave_function_collapse_lib/resources/island.tsx
+++ examples/minwebgl/wave_function_collapse_lib/resources/island.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="island" tilewidth="260" tileheight="260" tilecount="7" columns="1">
- <image source="D:/Job/cgtools/examples/minwebgl/wave_function_collapse/resources/tileset.png" width="260" height="1820"/>
+ <image source="tileset.png" width="260" height="1820"/>
 </tileset>
