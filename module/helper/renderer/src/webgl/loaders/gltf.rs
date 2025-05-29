3e0aeef698359fe492aff067cd94531d17109031
Bilan Vadim <vadimb892@gmail.com>
2025-05-29 13:06:39 +0300

    feat: create and bind object_id attribute

--- module/helper/renderer/src/webgl/loaders/gltf.rs
+++ module/helper/renderer/src/webgl/loaders/gltf.rs
@@ -1,7 +1,7 @@
 mod private
 {
   use std::{ cell::RefCell, rc::Rc };
-  use minwebgl::{ self as gl, JsCast, geometry::BoundingBox };
+  use minwebgl::{ self as gl, geometry::BoundingBox, JsCast, JsValue, WebglError };
   use crate::webgl::
   {
     ToFromGlEnum,
@@ -22,7 +22,7 @@ mod private
     TextureInfo, 
     WrappingMode
   };
-  use web_sys::wasm_bindgen::prelude::Closure;
+  use web_sys::{js_sys::Uint8Array, wasm_bindgen::prelude::Closure, WebGlBuffer};
 
   pub struct GLTF
   {
@@ -33,6 +33,54 @@ mod private
     pub textures : Vec< Rc< RefCell< Texture > > >,
     pub materials : Vec< Rc< RefCell< Material > > >,
     pub meshes : Vec< Rc< RefCell< Mesh > > >
+  }
+
+  fn upload_buffer_data< T >( 
+    gl : &gl::WebGl2RenderingContext, 
+    buffer : WebGlBuffer, 
+    target : u32, 
+    offset : u32, 
+    data : Vec< T > 
+  )
+  {
+    let data = data.iter()
+    .map( | i | i.to_be_bytes() )
+    .flatten()
+    .collect::< Vec< _ > >();
+
+    gl.bind_buffer( target, Some( &buffer ) );
+    gl.buffer_data_with_js_u8_array_and_src_offset_and_length
+    ( 
+      target, 
+      &UInt8Array::from( &data ), 
+      gl::STATIC_DRAW,
+      offset,
+      data.len() as u32
+    );
+  }
+
+  fn make_buffer_attibute_info< T >( 
+    buffer : web_sys::WebGlBuffer, 
+    offset : i32, 
+    stride : i32, 
+    slot : u32,
+    normalized : bool,
+    vector: gl::VectorDataType
+  ) -> AttributeInfo
+  {
+    let descriptor = gl::BufferDescriptor::new::< T >()
+    .offset( offset / vector.scalar().byte_size() )
+    .normalized( normalized )
+    .stride( stride / vector.scalar().byte_size() )
+    .vector( vector );
+
+    AttributeInfo
+    {
+      slot,
+      buffer,
+      descriptor,
+      bounding_box : Default::default()
+    }
   }
 
   pub async fn load
@@ -176,6 +224,7 @@ mod private
     let mut gl_buffers = Vec::new();
     // The target option may not be set for the attributes/indices buffers
     // This scenario should be checked
+
     for view in gltf_file.views()
     {
       let buffer = gl::buffer::create( &gl )?;
@@ -205,6 +254,9 @@ mod private
 
       gl_buffers.push( buffer );
     }
+
+    let object_id_buffer = gl::buffer::create( &gl )?;
+    gl_buffers.push( object_id_buffer );
 
     gl::info!( "GL Buffers: {}", gl_buffers.len() );
 
@@ -307,8 +359,11 @@ mod private
     materials.push( Rc::new( RefCell::new( Material::default() ) ) );
 
     gl::log::info!( "Materials: {}",materials.len() );
+    let mut stride = 0;
+    let mut offset = 0;
     let make_attibute_info = | acc : &gltf::Accessor< '_ >, slot |
     {
+      stride = acc.view().unwrap().stride().unwrap_or( 0 ) as i32 + 1;
       let data_type = match acc.data_type()
       {
         gltf::accessor::DataType::U8 => gl::DataType::U8,
@@ -319,11 +374,15 @@ mod private
         gltf::accessor::DataType::F32 => gl::DataType::F32
       };
 
+      let elements = 1;
+
       let descriptor = gl::BufferDescriptor::new::< [ f32; 1 ] >()
       .offset( acc.offset() as i32 / data_type.byte_size() )
       .normalized( acc.normalized() )
-      .stride( acc.view().unwrap().stride().unwrap_or( 0 ) as i32 / data_type.byte_size() )
-      .vector( gl::VectorDataType::new( data_type, acc.dimensions().multiplicity() as i32, 1 ) );
+      .stride( stride / data_type.byte_size() )
+      .vector( gl::VectorDataType::new( data_type, acc.dimensions().multiplicity() as i32, elements ) );
+
+      offset = acc.offset() as i32 + data_type.byte_size() * acc.dimensions().multiplicity() as i32 * elements;
 
       AttributeInfo
       {
@@ -333,101 +392,127 @@ mod private
         bounding_box : Default::default()
       }
     };
+
     let mut meshes = Vec::new();
+    let mut object_id : u32 = 0;
+    let mut object_id_data : Vec< u32 > = vec![];
+
     for gltf_mesh in gltf_file.meshes()
     {
-    let mut mesh = Mesh::default();
+      let mut mesh = Mesh::default();
 
-    for gltf_primitive in gltf_mesh.primitives()
-    {
-      let mut geometry = Geometry::new( gl )?;
-      geometry.draw_mode = gltf_primitive.mode().as_gl_enum();
-
-      // Indices
-      if let Some( acc ) = gltf_primitive.indices()
+      for gltf_primitive in gltf_mesh.primitives()
       {
-        let info = IndexInfo
-        {
-          buffer : gl_buffers[ acc.view().unwrap().index() ].clone(),
-          count : acc.count() as u32,
-          offset : acc.offset() as u32,
-          data_type : acc.data_type().as_gl_enum()
-        };
-        geometry.add_index( gl, info )?;
-      }
+        let mut geometry = Geometry::new( gl )?;
+        geometry.draw_mode = gltf_primitive.mode().as_gl_enum();
 
-      // Attributes
-      for ( sem, acc ) in gltf_primitive.attributes()
-      {
-        if acc.sparse().is_some()
+        // Indices
+        if let Some( acc ) = gltf_primitive.indices()
         {
-          gl::log::info!( "Sparce accessors are not supported yet" );
-          continue;
+          let info = IndexInfo
+          {
+            buffer : gl_buffers[ acc.view().unwrap().index() ].clone(),
+            count : acc.count() as u32,
+            offset : acc.offset() as u32,
+            data_type : acc.data_type().as_gl_enum()
+          };
+          geometry.add_index( gl, info )?;
         }
-        
-        match sem
-        {
-          gltf::Semantic::Positions => 
-          {
-            geometry.vertex_count = acc.count() as u32;
-            let gltf_box = gltf_primitive.bounding_box();
 
-            let mut attr_info = make_attibute_info( &acc, 0 );
-            attr_info.bounding_box = BoundingBox::new( gltf_box.min, gltf_box.max );
-            geometry.add_attribute( gl, "positions", attr_info, false )?;
-          },
-          gltf::Semantic::Normals => 
-          { 
-            geometry.add_attribute( gl, "normals", make_attibute_info( &acc, 1 ), false )?;
-          },
-          gltf::Semantic::TexCoords( i ) =>
+        // Attributes
+        for ( sem, acc ) in gltf_primitive.attributes()
+        {
+          if acc.sparse().is_some()
           {
-            assert!( i < 5, "Only 5 types of texture coordinates are supported" );
-            geometry.add_attribute
-            ( 
-              gl,
-              format!( "texture_coordinates_{}", 2 + i ), 
-              make_attibute_info( &acc, 2 + i ), 
-              false 
-            )?;
-          },
-          gltf::Semantic::Colors( i ) =>
+            gl::log::info!( "Sparce accessors are not supported yet" );
+            continue;
+          }
+
+          offset = 0;
+          
+          match sem
           {
-            assert!( i < 2, "Only 2 types of color coordinates are supported" );
-            geometry.add_attribute
-            ( 
-              gl,
-              format!( "colors_{}", 7 + i ), 
-              make_attibute_info( &acc, 7 + i ), 
-              false 
-            )?;
-          },
-          gltf::Semantic::Tangents =>
-          {
-            geometry.add_attribute
-            ( 
-              gl,
-              "tangents", 
-              make_attibute_info( &acc, 9 ), 
-              true 
-            )?;
-          },
-          a => { gl::warn!( "Unsupported attribute: {:?}", a ); continue; }
+            gltf::Semantic::Positions => 
+            {
+              geometry.vertex_count = acc.count() as u32;
+              let gltf_box = gltf_primitive.bounding_box();
+
+              let mut attr_info = make_attibute_info( &acc, 0 );
+              attr_info.bounding_box = BoundingBox::new( gltf_box.min, gltf_box.max );
+              geometry.add_attribute( gl, "positions", attr_info, false )?;
+            },
+            gltf::Semantic::Normals => 
+            { 
+              geometry.add_attribute( gl, "normals", make_attibute_info( &acc, 1 ), false )?;
+            },
+            gltf::Semantic::TexCoords( i ) =>
+            {
+              assert!( i < 5, "Only 5 types of texture coordinates are supported" );
+              geometry.add_attribute
+              ( 
+                gl,
+                format!( "texture_coordinates_{}", 2 + i ), 
+                make_attibute_info( &acc, 2 + i ), 
+                false 
+              )?;
+            },
+            gltf::Semantic::Colors( i ) =>
+            {
+              assert!( i < 2, "Only 2 types of color coordinates are supported" );
+              geometry.add_attribute
+              ( 
+                gl,
+                format!( "colors_{}", 7 + i ), 
+                make_attibute_info( &acc, 7 + i ), 
+                false 
+              )?;
+            },
+            gltf::Semantic::Tangents =>
+            {
+              geometry.add_attribute
+              ( 
+                gl,
+                "tangents", 
+                make_attibute_info( &acc, 9 ), 
+                true 
+              )?;
+            },
+            a => { gl::warn!( "Unsupported attribute: {:?}", a ); continue; }
+          };
+        }
+
+        let object_id_attribute_info = make_buffer_attibute_info::< [ F32; 1 ] >( 
+          object_id_buffer, 
+          offset, 
+          stride, 
+          10, 
+          false, 
+          gl::VectorDataType::new( mingl::DataType::U32, 1, 1 ) 
+        );
+
+        geometry.add_attribute
+        ( 
+          gl,
+          "object_id", 
+          object_id_attribute_info, 
+          false 
+        )?;
+
+        let material_id = gltf_primitive.material().index().unwrap_or( materials.len() - 1 );
+        let primitive = Primitive
+        {
+          geometry : Rc::new( RefCell::new( geometry ) ),
+          material : materials[ material_id ].clone()
         };
+
+        mesh.add_primitive( Rc::new( RefCell::new( primitive ) ) );
       }
 
-      let material_id = gltf_primitive.material().index().unwrap_or( materials.len() - 1 );
-      let primitive = Primitive
-      {
-        geometry : Rc::new( RefCell::new( geometry ) ),
-        material : materials[ material_id ].clone()
-      };
-
-      mesh.add_primitive( Rc::new( RefCell::new( primitive ) ) );
+      meshes.push( Rc::new( RefCell::new( mesh ) ) );
+      object_id += 1;
     }
 
-    meshes.push( Rc::new( RefCell::new( mesh ) ) );
-    }
+    upload_buffer_data( gl, object_id_buffer, gl::ARRAY_BUFFER, 0, object_id_data );
 
     gl::log::info!( "Meshes: {}",meshes.len() );
 
