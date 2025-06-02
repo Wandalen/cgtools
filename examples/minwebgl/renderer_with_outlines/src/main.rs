0178aeb0530da64088d1c3ad34731445ea2602d5
Bilan Vadim <vadimb892@gmail.com>
2025-06-02 11:21:23 +0300

    feat: add gbuffer to example `renderer with outlines`

--- examples/minwebgl/renderer_with_outlines/src/main.rs
+++ examples/minwebgl/renderer_with_outlines/src/main.rs
@@ -1,13 +1,29 @@
 
+use std::collections::{HashMap, HashSet};
+
 use minwebgl as gl;
 use rand::Rng;
 use renderer::webgl::
 {
-  post_processing::{self, Pass, SwapFramebuffer, outline::{ OutlinePass, MAX_OBJECT_COUNT } }, Camera, Renderer
+  post_processing::{self, Pass, SwapFramebuffer, GBuffer, GBufferAttachment, outline::{ OutlinePass, MAX_OBJECT_COUNT } }, Camera, Renderer
 };
 
 mod camera_controls;
 mod loaders;
+
+fn random_color( rng : &mut rand::Rng ) -> F32x4
+{
+  let range = 0.2..1.0;
+  F32x4::from
+  (
+    [ 
+      rng.random_range( range.clone() ), 
+      rng.random_range( range.clone() ),
+      rng.random_range( range.clone() ),
+      1.0
+    ] 
+  )
+}
 
 fn generate_object_colors( object_count : usize ) -> Vec< [ f32; 4 ] > 
 {
@@ -19,7 +35,8 @@ fn generate_object_colors( object_count : usize ) -> Vec< [ f32; 4 ] >
   ( 
     | i |
     {
-      object_colors[ i ] = [ 
+      object_colors[ i ] = 
+      [ 
         rng.random_range( range.clone() ), 
         rng.random_range( range.clone() ),
         rng.random_range( range.clone() ),
@@ -55,7 +72,7 @@ async fn run() -> Result< (), gl::WebglError >
   let aspect_ratio = width / height;
   let fov = 70.0f32.to_radians();
   let near = 0.001;
-  let far = 100.0;
+  let far = 1000.0;
 
   let mut camera = Camera::new( eye, up, center, aspect_ratio, fov, near, far );
   camera.set_window_size( [ width, height ].into() );
@@ -70,6 +87,21 @@ async fn run() -> Result< (), gl::WebglError >
   renderer.set_use_emission( true );
   renderer.set_ibl( loaders::ibl::load( &gl, "envMap" ).await );
 
+  let b = gltf.gl_buffers.clone();
+  let buffers = [
+    vec![ b[ 0 ] ], 
+    vec![ b[ 7 ] ], 
+    vec![ b[ 1 ] ],
+    vec![ b[ 10 ], b[ 11 ], b[ 2 ]  ],
+    vec![ b[ 12 ] ]
+  ];
+
+  let attachments = post_processing::gbuffer::ALL.iter()
+  .zip( buffers )
+  .collect::< HashMap< _, _ > >();
+
+  let mut gbuffer = Gbuffer::new( gl, canvas.width(), canvas.height(), attachments );
+
   let mut swap_buffer = SwapFramebuffer::new( &gl, canvas.width(), canvas.height() );
 
   let tonemapping = post_processing::ToneMappingPass::< post_processing::ToneMappingAces >::new( &gl )?;
@@ -77,8 +109,8 @@ async fn run() -> Result< (), gl::WebglError >
   let mut outline = OutlinePass::new
   ( 
     &gl, 
-    renderer.get_depth_texture(), 
-    renderer.get_object_id_texture(), 
+    gbuffer.get_texture( GBufferAttachment::Normal ), 
+    gbuffer.get_texture( GBufferAttachment::PbrInfo ), 
     2.0, 
     canvas.width(), 
     canvas.height() 
@@ -100,6 +132,9 @@ async fn run() -> Result< (), gl::WebglError >
       renderer.render( &gl, &mut scenes[ 0 ].borrow_mut(), &camera )
       .expect( "Failed to render" );
 
+      gbuffer.render( &gl, &mut scenes[ 0 ].borrow_mut(), &camera )
+      .expect( "Failed to render gbuffer" );
+
       swap_buffer.reset();
       swap_buffer.bind( &gl );
       swap_buffer.set_input( renderer.get_main_texture() );
@@ -111,7 +146,7 @@ async fn run() -> Result< (), gl::WebglError >
       swap_buffer.swap();
     
       let t = to_srgb.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
-      .expect( "Failed to render ToSrgbPass" );
+      .expect( "Failed to render to srgb pass" );
 
       swap_buffer.set_output( t );
       swap_buffer.swap();
@@ -119,7 +154,7 @@ async fn run() -> Result< (), gl::WebglError >
       let outline_thickness = ( 2.0 * ( time / 1000.0 ).sin().abs() ) as f32;
       outline.set_outline_thickness( outline_thickness );
       let _ = outline.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
-      .expect( "Failed to render OutlinePass" );
+      .expect( "Failed to render outline pass" );
 
       true
     }
