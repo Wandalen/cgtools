3308b536369b47a0f54b6c74f94e5a0f4b930ff0
Bilan Vadim <vadimb892@gmail.com>
2025-06-12 00:01:01 +0300

    feat: change camera state to adapt it for model size; change object color calculation; change post effect pipeline; add choice which gbuffer to renderer

--- examples/minwebgl/renderer_with_outlines/src/main.rs
+++ examples/minwebgl/renderer_with_outlines/src/main.rs
@@ -19,31 +19,15 @@ use renderer::webgl::
       NarrowOutlinePass, 
       MAX_OBJECT_COUNT 
     }, 
-    /*upload_buffer_data,*/ 
     GBuffer, 
     GBufferAttachment, 
     Pass, 
     SwapFramebuffer
   }
 };
-//use gl::math::F32x4;
 
 mod camera_controls;
 mod loaders;
-
-// fn random_color( rng : &mut impl rand::Rng ) -> F32x4
-// {
-//   let range = 0.2..1.0;
-//   F32x4::from
-//   (
-//     [ 
-//       rng.random_range( range.clone() ), 
-//       rng.random_range( range.clone() ),
-//       rng.random_range( range.clone() ),
-//       1.0
-//     ] 
-//   )
-// }
 
 fn generate_object_colors() -> Vec< [ f32; 4 ] > 
 {
@@ -80,7 +64,7 @@ fn get_attributes( gltf : &GLTF ) -> Result< HashMap< Box< str >, AttributeInfo 
     }
   }
 
-  Err( gl::WebglError::MissingDataError( "Primitive".to_string() ) )
+  Err( gl::WebglError::MissingDataError( "Primitive" ) )
 }
 
 async fn run() -> Result< (), gl::WebglError >
@@ -98,45 +82,74 @@ async fn run() -> Result< (), gl::WebglError >
   let width = canvas.width() as f32;
   let height = canvas.height() as f32;
 
+  let gltf_path = "old_rusty_car.glb";
+  let mut gltf = renderer::webgl::loaders::gltf::load( &document, gltf_path, &gl ).await?;
+  let scenes = gltf.scenes.clone();
+
   // Camera setup
-  let mut eye = gl::math::F32x3::from( [ 0.0, 20.0, 20.0 ] );
-  eye /= 500.0;
+  let scene_bounding_box = scenes[ 0 ].borrow().bounding_box();
+  gl::info!( "Boudnig box: {:?}", scene_bounding_box );
+  let diagonal = ( scene_bounding_box.max - scene_bounding_box.min ).mag();
+  let dist = scene_bounding_box.max.mag();
+  let exponent = 
+  {
+    let bits = diagonal.to_bits();
+    let exponent_field = ( ( bits >> 23 ) & 0xFF ) as i32;
+    exponent_field - 127
+  };
+  gl::info!( "Exponent: {:?}", exponent );
+
+  // Camera setup
+  let mut eye = gl::math::F32x3::from( [ 0.0, 1.0, 1.0 ] );
+  eye *= dist;
   let up = gl::math::F32x3::from( [ 0.0, 1.0, 0.0 ] );
-  let center = gl::math::F32x3::from( [ 0.0, 0.0, 0.0 ] );
+  let center = scene_bounding_box.center();
 
   let aspect_ratio = width / height;
   let fov = 70.0f32.to_radians();
-  let near = 0.001;
-  let far = 1000.0;
+  let near = 1.0 * 10.0f32.powi( exponent ).min( 1.0 );
+  let far = near * 10.0f32.powi( exponent.abs() );
 
   let mut camera = Camera::new( eye, up, center, aspect_ratio, fov, near, far );
   camera.set_window_size( [ width, height ].into() );
 
   camera_controls::setup_controls( &canvas, &camera.get_controls() );
 
-  let gltf_path = "dodge-challenger/gltf";
-  let mut gltf = renderer::webgl::loaders::gltf::load( &document, gltf_path, &gl ).await?;
-  let scenes = gltf.scenes.clone();
-
-  let _ = add_attributes( 
+  let object_id_data = add_attributes( 
     &gl, 
     &mut gltf, 
     HashSet::from( [ GBufferAttachment::PbrInfo ] ) 
   )?;
-  
-  let object_count = scenes[ 0 ].as_ref().borrow().children.len();
-  let mut object_color_id_data = vec![ 0_u32; MAX_OBJECT_COUNT ];
-  ( 0..object_count ).for_each( | i | object_color_id_data[ i ] = i as u32 + 1 );
-  let object_color_id_data = object_color_id_data.iter().map( | i | i.to_be_bytes() ).flatten().collect::< Vec< _ > >();
-  let object_color_id_buffer = add_buffer( &gl, &mut gltf, object_color_id_data )?;
+
+  // let object_count = scenes[ 0 ].as_ref().borrow().children.len();
+  // let object_color_id_data = object_id_data.into_iter()
+  // .map( 
+  //   | i |
+  //   {
+  //     let j = i % MAX_OBJECT_COUNT as u32;
+  //     ( if j == 0
+  //     {
+  //       1
+  //     } 
+  //     else
+  //     {
+  //       j
+  //     } ) as f32
+  //   } 
+  // )
+  // .collect::< Vec< _ > >();
+
+  // let object_color_id_data = object_color_id_data.iter().map( | i | i.to_be_bytes() ).flatten().collect::< Vec< _ > >();
+  // let object_color_id_buffer = add_buffer( &gl, &mut gltf, object_color_id_data )?;
 
   let mut renderer = Renderer::new( &gl, canvas.width(), canvas.height(), 4 )?;
-  renderer.set_use_emission( true );
+  //renderer.set_use_emission( true );
   renderer.set_ibl( loaders::ibl::load( &gl, "envMap" ).await );
 
   let attributes = get_attributes( &gltf )?;  
 
   gl::info!( "{:?}", attributes.keys() );
+  
   let get_buffer = | name | attributes.get( name ).unwrap().buffer.clone();
 
   let mut gl_buffers_iter = gltf.gl_buffers.iter().rev();
@@ -146,7 +159,8 @@ async fn run() -> Result< (), gl::WebglError >
   let attachments = HashMap::from(
     [
       ( GBufferAttachment::Position, vec![ get_buffer( "positions" ) ] ),
-      ( GBufferAttachment::Albedo, vec![ get_buffer( "colors_7" ) ] ),
+      //( GBufferAttachment::Albedo, vec![] ),
+      //( GBufferAttachment::Color, vec![ get_buffer( "colors_7" ) ] ),
       ( GBufferAttachment::Normal, vec![ get_buffer( "normals" ) ] ),
       ( 
         GBufferAttachment::PbrInfo, 
@@ -156,11 +170,16 @@ async fn run() -> Result< (), gl::WebglError >
           get_buffer( "texture_coordinates_2" )
         ] 
       ),
-      ( GBufferAttachment::ObjectColorId, vec![ object_color_id_buffer ] )
+      //( GBufferAttachment::ObjectColorId, vec![ object_color_id_buffer ] )
     ]
   );
 
-  let mut gbuffer = GBuffer::new( &gl, canvas.width(), canvas.height(), attachments )?;
+  let mut gbuffer = GBuffer::new( 
+    &gl, 
+    canvas.width(), 
+    canvas.height(), 
+    attachments 
+  )?;
 
   let mut swap_buffer = SwapFramebuffer::new( &gl, canvas.width(), canvas.height() );
 
@@ -179,6 +198,8 @@ async fn run() -> Result< (), gl::WebglError >
   let object_colors = generate_object_colors();
   outline.set_object_colors( &gl, object_colors );
 
+  let render_attachment = None; //Some( GBufferAttachment::Position );
+
   scenes[ 0 ].borrow_mut().update_world_matrix();
   // Define the update and draw logic
   let update_and_draw =
@@ -186,36 +207,44 @@ async fn run() -> Result< (), gl::WebglError >
     move | t : f64 |
     {
       // If textures are of different size, gl.view_port needs to be called
-      let _time = t as f32 / 1000.0;
-
-      renderer.render( &gl, &mut scenes[ 0 ].borrow_mut(), &camera )
-      .expect( "Failed to render" );
+      let time = t as f32 / 1000.0;
 
       gbuffer.render( &gl, &mut scenes[ 0 ].borrow_mut(), &camera )
       .expect( "Failed to render gbuffer" );
+
+      renderer.render( &gl, &mut scenes[ 0 ].borrow_mut(), &camera )
+      .expect( "Failed to render" );
 
       swap_buffer.reset();
       swap_buffer.bind( &gl );
       swap_buffer.set_input( renderer.get_main_texture() );
 
+      let outline_thickness = 2.0;//( 2.0 * ( time / 1000.0 ).sin().abs() ) as f32;
+      outline.set_outline_thickness( outline_thickness );
+      let t = outline.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
+      .expect( "Failed to render outline pass" );
+
+      let t = if let Some( render_attachment ) = render_attachment
+      { 
+        Some( gbuffer.get_texture( render_attachment ).unwrap() )
+      }
+      else
+      {
+        t
+      };
+
+      swap_buffer.bind( &gl );
+      swap_buffer.set_output( t );
+      swap_buffer.swap();
+
       let t = tonemapping.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
       .expect( "Failed to render tonemapping pass" );
 
-      swap_buffer.bind( &gl );
       swap_buffer.set_output( t );
       swap_buffer.swap();
     
       let _t = to_srgb.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
       .expect( "Failed to render to srgb pass" );
-
-      // swap_buffer.bind( &gl );
-      // swap_buffer.set_output( t );
-      // swap_buffer.swap();
-
-      // let outline_thickness = ( 2.0 * ( time / 1000.0 ).sin().abs() ) as f32;
-      // outline.set_outline_thickness( outline_thickness );
-      // let _ = outline.render( &gl, swap_buffer.get_input(), swap_buffer.get_output() )
-      // .expect( "Failed to render outline pass" );
 
       true
     }
