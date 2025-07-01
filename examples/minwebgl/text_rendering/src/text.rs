a0ddab485bf31b3abf46ba000633aa01cc3f916c
Bilan Vadim <vadimb892@gmail.com>
2025-07-01 12:37:22 +0300

    fix: text_rendering

--- examples/minwebgl/text_rendering/src/text.rs
+++ examples/minwebgl/text_rendering/src/text.rs
@@ -10,8 +10,6 @@ pub mod ufo
   use minwebgl as gl;
   use gl::{ F32x3, math::vector::cross };
   use quick_xml::{ Reader, events::Event };
-  use i_float::int::point::IntPoint;
-  use i_triangle::int::triangulatable::IntTriangulatable;
   use crate::
   { 
     AttributesData, PrimitiveData, Transform 
@@ -419,37 +417,66 @@ pub mod ufo
 
     contours.swap( body_id, 0 );
 
-    let contours = contours.into_iter()
-    .map( 
-      | c | 
+    ///////////////////////////////////
+
+    let mut flat_positions: Vec< f64 > = Vec::new();
+    let mut hole_indices: Vec< usize > = Vec::new();
+
+    if let Some( outer_contour ) = contours.get( 0 ) 
+    {
+      if outer_contour.is_empty() 
       {
-        c.into_iter()
-        .map( 
-          | [ x, y ] |
-          {
-            IntPoint
-            {
-              x : x as i32, 
-              y : y as i32
-            }
-          } 
-        )
-        .collect::< Vec< _ > >()
-      } 
-    )
+        return None;
+      }
+      for &[ x, y ] in outer_contour 
+      {
+        flat_positions.push( x as f64 );
+        flat_positions.push( y as f64 );
+      }
+    } 
+    else 
+    {
+      return None;
+    }
+
+    // Process holes (remaining contours)
+    // Their winding order must be opposite to the outer (e.g., CW for holes)
+    for i in 1..contours.len() 
+    {
+      let hole_contour = &contours[ i ];
+      if hole_contour.is_empty() 
+      {
+        continue;
+      }
+
+      hole_indices.push( flat_positions.len() / 2 );
+
+      for &[ x, y ] in hole_contour 
+      {
+        flat_positions.push( x as f64 );
+        flat_positions.push( y as f64 );
+      }
+    }
+
+    // Perform triangulation
+    let Ok( indices ) = earcutr::earcut( &flat_positions, &hole_indices, 2 ) 
+    else
+    {
+      return None;
+    };
+
+    let indices = indices.into_iter()
+    .map( | i | i as u32 )
     .collect::< Vec< _ > >();
 
-    let triangulation = contours.triangulate().to_triangulation::< u32 >();
-
-    let flat_positions = triangulation.points;
-    let mut indices = triangulation.indices;
+    ///////////////////
 
     // Create two surface of glyph
-    let mut positions = flat_positions.iter()
+    let mut positions = flat_positions.chunks( 2 )
     .map(
       | p |
       {
-        [ p.x as f32, p.y as f32, 0.5 ]
+        [ p[ 0 ] as f32, p[ 1 ] as f32, 0.5 ]
       }
     )
     .collect::< Vec< _ > >();
