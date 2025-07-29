2b8970c2051ca46f1dc3ea8ae319c3ebee4531fb
JekiXD <ambrejine00@gmail.com>
2025-07-29 10:49:31 +0300

    Added simple 3d line

--- examples/minwebgl/3d_line/src/main.rs
+++ examples/minwebgl/3d_line/src/main.rs
@@ -1,3 +1,4 @@
+use mingl::CameraOrbitControls;
 use minwebgl::{self as gl, IntoArray};
 use gl::GL;
 use std::
@@ -6,17 +7,30 @@ use std::
   rc::Rc,
 };
 
-fn generate_sample_points_interleaved( width : f32, height : f32 ) -> [ [ f32; 2 ]; 8 ]
+mod camera_controls;
+
+fn generate_sample_points( num : usize ) -> Vec< gl::F32x3 >
 {
-  let stepx = width / 9.0;
-  let stepy = height / 3.0;
-  let mut points = [ [ 0.0; 2 ]; 8 ];
-  let mut i = 0;
-  for x in ( 1..9 ).step_by( 2 )
+  let mut points = Vec::with_capacity( num );
+  let mut pos = gl::F32x3::default();
+  let scale = 0.1;
+  for i in 0..num
   {
-    points[ i ] = [ ( x as f32 + 0.0 ) * stepx - width / 2.0, 1.0 * stepy - height / 2.0];
-    points[ i + 1 ] = [ ( x as f32 + 1.0 ) * stepx - width / 2.0, 2.0 * stepy - height / 2.0];
-    i += 2;
+    let k = i % 3;
+    if k == 0 
+    {
+      pos += gl::F32x3::X * scale;
+    }
+    else if k == 1 
+    {
+      pos += gl::F32x3::Z * scale;    
+    } 
+    else 
+    {
+      pos += gl::F32x3::Y * scale;    
+    }
+
+    points.push( pos );
   }
 
   return points;
@@ -33,30 +47,44 @@ fn run() -> Result< (), gl::WebglError >
 
   let fragment_shader_src = include_str!( "../shaders/main.frag" );
 
-  let projection_matrix = gl::math::mat3x3h::orthographic_rh_gl( -width / 2.0, width / 2.0, -height / 2.0, height / 2.0, 0.0, 1.0 );
-  let line_width = 50.0;
+  // Camera setup
+  let eye = gl::math::F32x3::from( [ 0.0, 1.0, 1.0 ] );
+  let up = gl::math::F32x3::from( [ 0.0, 1.0, 0.0 ] );
+  let center = gl::math::F32x3::default();
 
-  let points = generate_sample_points_interleaved( width, height );
+  let aspect_ratio = width / height;
+  let fov = 70.0f32.to_radians();
+  let near = 0.0001f32;
+  let far = 100.0f32;
 
-  let mut line = line_tools::d2::Line::default();
-  line.join = line_tools::Join::Round( 50 );
-  line.cap = line_tools::Cap::Round( 50 );
-
-  for p in points
+  let camera = CameraOrbitControls
   {
-    line.points.push( p.into() );
-  }
+    eye : eye,
+    up : up,
+    center : center,
+    window_size : [ width, height ].into(),
+    fov,
+    ..Default::default()
+  };
+  let camera = Rc::new( RefCell::new( camera ) );
+  camera_controls::setup_controls( &canvas, &camera );
 
-  line.create_mesh( &gl, fragment_shader_src )?;
+  let projection_matrix = gl::math::mat3x3h::perspective_rh_gl( fov, aspect_ratio, near, far );
+
+  let line_width = 0.02;
+
+  let points = generate_sample_points( 2 );
+
+  let mut line = line_tools::d3::Line::default();
+  line.points = points;
+
+  line.create_mesh( &gl, 16, fragment_shader_src )?;
   let mesh = line.get_mesh();
-  mesh.upload_matrix( &gl, "u_projection_matrix", &projection_matrix.to_array() )?;
   mesh.upload( &gl, "u_width", &line_width )?;
-
   mesh.upload( &gl, "u_color", &[ 1.0, 1.0, 1.0 ] )?;
-
-  // mesh.upload_to( &gl, "body", "u_color", &[ 1.0, 1.0, 1.0 ] )?;
-  // mesh.upload_to( &gl, "join", "u_color", &[ 1.0, 0.0, 0.0 ] )?;
-  // mesh.upload_to( &gl, "cap", "u_color", &[ 0.0, 1.0, 0.0 ] )?;
+  mesh.upload( &gl, "u_resolution", &[ width as f32, height as f32 ] )?;
+  mesh.upload_matrix( &gl, "u_projection_matrix", &projection_matrix.to_array() )?;
+  mesh.upload_matrix( &gl, "u_world_matrix", &gl::math::mat4x4::identity().to_array() ).unwrap();
   
   // Define the update and draw logic
   let update_and_draw =
@@ -66,11 +94,14 @@ fn run() -> Result< (), gl::WebglError >
     {
       let _time = t as f32 / 1000.0;
 
-      let scale = [ ( ( _time * 2.0 ).sin().abs() + 0.1 ) * 2.0, 1.0 ];
-      let rotation = 0.0;
-      let translation = gl::F32x2::default();
-      let world_matrix = gl::F32x3x3::from_scale_rotation_translation( scale, rotation, translation.as_array() );
-      line.get_mesh().upload_matrix( &gl, "u_world_matrix", &world_matrix.to_array() ).unwrap();
+      let scale = [ ( ( _time * 2.0 ).sin().abs() + 0.1 ) * 2.0, 1.0, 1.0 ];
+      let rotation = gl::QuatF32::from_angle_x( ( _time * 2.0 ).sin() );
+      let translation = gl::F32x3::default();
+      let world_matrix = gl::F32x4x4::from_scale_rotation_translation( scale, rotation, translation.as_array() );
+
+      let mesh = line.get_mesh();
+      //mesh.upload_matrix( &gl, "u_world_matrix", &world_matrix.to_array() ).unwrap();
+      mesh.upload_matrix( &gl, "u_view_matrix", &camera.borrow().view().to_array() ).unwrap();
       
       line.draw( &gl ).unwrap();
 
--- module/helper/line_tools/src/d3/line.rs
+++ module/helper/line_tools/src/d3/line.rs
@@ -1,154 +1,98 @@
 mod private
 {
-  // use crate::*;
-  // use minwebgl::{self as gl, IntoArray};
-  // use ndarray_cg as math;
+  use crate::*;
+  use minwebgl::{self as gl, IntoArray};
+  use ndarray_cg as math;
 
-  // #[ derive( Debug, Clone, Default ) ]
-  // pub struct Line
-  // {
-  //   pub points : Vec< math::F32x2 >,
-  //   mesh : Option< Mesh >
-  // }
-  // impl Line
-  // {
-  //   pub fn create_mesh( &mut self, gl : &gl::WebGl2RenderingContext, fragment_shder : &str ) -> Result< (), gl::WebglError >
-  //   {
-  //     let points : Vec< f32 > = self.points.iter().flat_map( | p | p.to_array() ).collect();
-  //     let ( round_geometry_list, round_geometry_count ) = helpers::round_geometry( 32 );
-
-  //     // Buffers
-  //     let body_buffer = gl.create_buffer().expect( "Failed to create a buffer" );
-
-  //     gl.bind_buffer( gl::ARRAY_BUFFER, Some( &body_instanced_buffer ) );
-  //     gl::buffer::upload( gl, &body_instanced_buffer, &helpers::BODY_GEOMETRY, gl::STATIC_DRAW );
-
-     
-
-      
-
-  //     // Body vao
-  //     let body_vao = gl.create_vertex_array();
-  //     gl.bind_vertex_array( body_vao.as_ref() );
-
-  //     gl::BufferDescriptor::new::< [ f32; 2 ] >().offset( 0 ).stride( 2 ).divisor( 0 ).attribute_pointer( &gl, 0, &body_instanced_buffer )?;
-  //     gl::BufferDescriptor::new::< [ f32; 2 ] >().offset( 0 ).stride( 2 ).divisor( 1 ).attribute_pointer( &gl, 1, &body_buffer )?;
-  //     gl::BufferDescriptor::new::< [ f32; 2 ] >().offset( 2 ).stride( 2 ).divisor( 1 ).attribute_pointer( &gl, 2, &body_buffer )?;
+  #[ derive( Debug, Clone, Default ) ]
+  pub struct Line
+  {
+    pub points : Vec< math::F32x3 >,
+    mesh : Option< Mesh >
+  }
+  impl Line
+  {
+    pub fn create_mesh( &mut self, gl : &gl::WebGl2RenderingContext, segments : u32, fragment_shader : &str ) -> Result< (), gl::WebglError >
+    {
+      let body_geometry : Vec< [ f32; 3 ] > = helpers::BODY_GEOMETRY.into_iter()
+      .map( | v | { [ 0.0, v[ 1 ], v[ 0 ] ] } )
+      .collect();
+      let circle_left_half_geometry : Vec< [ f32; 3 ]> = helpers::circle_left_half_geometry( segments as usize )
+      .into_iter()
+      .map( | v | { [ v[ 0 ], v[ 1 ], 0.0 ] } )
+      .collect();
+      let circle_right_half_geometry : Vec< [ f32; 3 ]> = helpers::circle_right_half_geometry( segments as usize )
+      .into_iter()
+      .map( | v | { [ v[ 0 ], v[ 1 ], 1.0 ] } )
+      .collect();
 
 
-  //     // Programs
-  //     let body_program = gl::ProgramFromSources::new( include_str!( "./shaders/body.vert" ), fragment_shder ).compile_and_link( &gl )?;
-  //     let b_program = Program
-  //     {
-  //       vao : body_vao,
-  //       program : body_program,
-  //       draw_mode : gl::TRIANGLES,
-  //       instance_count : Some( ( self.points.len() - 1 ) as u32 ),
-  //       index_count : None,
-  //       vertex_count : helpers::BODY_GEOMETRY.len() as u32
-  //     };
+      let points : Vec< f32 > = self.points.iter().flat_map( | p | p.to_array() ).collect();
 
-  //     let join_vert =
-  //     match self.join 
-  //     {
-  //       Join::Round( _ ) => include_str!( "./shaders/round_join.vert" ),
-  //       Join::Miter => include_str!( "./shaders/miter_join.vert" ),
-  //       Join::Bevel => include_str!( "./shaders/bevel_join.vert" )
-  //     };
+      let body_count = body_geometry.len();
+      let circle_left_half_count = circle_left_half_geometry.len();
+      let circle_right_half_count = circle_right_half_geometry.len();
 
-  //     let ( cap_vert, cap_draw_mode ) =
-  //     match self.cap
-  //     {
-  //       Cap::Round( _ ) =>
-  //       {
-  //         ( include_str!( "./shaders/round_cap.vert" ), gl::TRIANGLE_FAN )
-  //       },
-  //       Cap::Square =>
-  //       {
-  //         ( include_str!( "./shaders/square_cap.vert" ), gl::TRIANGLES )
-  //       }
-  //       _ => { ( include_str!( "./shaders/empty.vert" ), gl::TRIANGLES ) }
-  //     };
+      let vertex_count = body_count + circle_left_half_count + circle_right_half_count;
 
-  //     let join_program = gl::ProgramFromSources::new( join_vert, fragment_shder ).compile_and_link( &gl )?;
-  //     let cap_program = gl::ProgramFromSources::new( cap_vert, fragment_shder ).compile_and_link( &gl )?;
+      let mut geometry = Vec::new();
+      geometry.extend_from_slice( &circle_left_half_geometry );
+      geometry.extend_from_slice( &body_geometry );
+      geometry.extend_from_slice( &circle_right_half_geometry );
 
-  //     let j_program = Program
-  //     {
-  //       vao : join_vao,
-  //       program : join_program,
-  //       draw_mode : gl::TRIANGLE_FAN,
-  //       instance_count : Some( ( self.points.len() - 2 ) as u32 ),
-  //       index_count : None,
-  //       vertex_count : join_geometry_count as u32
-  //     };
 
-  //     let c_program = Program
-  //     {
-  //       vao : cap_vao,
-  //       program : cap_program,
-  //       draw_mode : cap_draw_mode,
-  //       instance_count : None,
-  //       index_count : None,
-  //       vertex_count : cap_geometry_count as u32
-  //     };
+      let buffer = gl.create_buffer().expect( "Failed to create a buffer" );
+      let instanced_buffer = gl.create_buffer().expect( "Failed to create a instanced_buffer" );
 
-  //     let mut mesh = Mesh::default();
-  //     mesh.add_program( "cap", c_program );
-  //     mesh.add_program( "join", j_program );
-  //     mesh.add_program( "body", b_program );
+      gl::buffer::upload( gl, &buffer, &points, gl::DYNAMIC_DRAW );
+      gl::buffer::upload( gl, &instanced_buffer, &geometry, gl::STATIC_DRAW );
 
-  //     self.mesh = Some( mesh );
+      let vao = gl.create_vertex_array();
+      gl.bind_vertex_array( vao.as_ref() );
 
-  //     Ok( () )
-  //   }
+      gl::BufferDescriptor::new::< [ f32; 3 ] >().stride( 3 ).offset( 0 ).divisor( 0 ).attribute_pointer( gl, 0, &instanced_buffer )?;
+      gl::BufferDescriptor::new::< [ f32; 3 ] >().stride( 3 ).offset( 0 ).divisor( 1 ).attribute_pointer( gl, 1, &buffer )?;
+      gl::BufferDescriptor::new::< [ f32; 3 ] >().stride( 3 ).offset( 3 ).divisor( 1 ).attribute_pointer( gl, 2, &buffer )?;
 
-  //   pub fn draw( &self, gl : &gl::WebGl2RenderingContext ) -> Result< (), gl::WebglError >
-  //   {
-  //     let mesh = self.mesh.as_ref().expect( "Mesh has not been created yet" );
-  //     mesh.draw( gl, "body" );
-  //     mesh.draw( gl, "join" );
+      let program = gl::ProgramFromSources::new( include_str!( "./shaders/merged.vert" ), fragment_shader ).compile_and_link( gl )?;
+      let program = Program
+      {
+        vao : vao,
+        program : program,
+        draw_mode : gl::TRIANGLES,
+        instance_count : Some( ( self.points.len() - 1 ) as u32 ),
+        index_count : None,
+        vertex_count : vertex_count as u32
+      };
 
-  //     match self.cap
-  //     {
-  //       Cap::Round( _ ) => 
-  //       {
-  //         mesh.upload_to( gl, "cap", "u_inPoint", self.points[ 0 ].as_slice() )?;
-  //         mesh.draw( gl, "cap" );
+      let mut mesh = Mesh::default();
+      mesh.add_program( "body", program );
 
-  //         mesh.upload_to( gl, "cap", "u_inPoint", self.points[ self.points.len() - 1 ].as_slice() )?;
-  //         mesh.draw( gl, "cap" );
-  //       },
-  //       Cap::Square =>
-  //       {
-  //         mesh.upload_to( gl, "cap", "u_inPointA", self.points[ 1 ].as_slice() )?;
-  //         mesh.upload_to( gl, "cap", "u_inPointB", self.points[ 0 ].as_slice() )?;
-  //         mesh.draw( gl, "cap" );
+      self.mesh = Some( mesh );
 
-  //         let len = self.points.len();
+      Ok( () )
+    }
 
-  //         mesh.upload_to( gl, "cap", "u_inPointA", self.points[ len - 2 ].as_slice() )?;
-  //         mesh.upload_to( gl, "cap", "u_inPointB", self.points[ len - 1 ].as_slice() )?;
-  //         mesh.draw( gl, "cap" );
-  //       },
-  //       _ => {}
-  //     }
+    pub fn draw( &self, gl : &gl::WebGl2RenderingContext ) -> Result< (), gl::WebglError >
+    {
+      let mesh = self.mesh.as_ref().expect( "Mesh has not been created yet" );
+      mesh.draw( gl, "body" );
 
-  //     Ok( () )
-  //   }
+      Ok( () )
+    }
 
-  //   pub fn get_mesh( &self ) -> &Mesh
-  //   {
-  //     self.mesh.as_ref().expect( "Mesh has not been created yet" )
-  //   }    
-  // }
+    pub fn get_mesh( &self ) -> &Mesh
+    {
+      self.mesh.as_ref().expect( "Mesh has not been created yet" )
+    }     
+  }
 }
 
 crate::mod_interface!
 {
 
-  // orphan use
-  // {
-  //   Line
-  // };
+  orphan use
+  {
+    Line
+  };
 }
