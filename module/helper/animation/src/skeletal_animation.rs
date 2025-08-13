30f8a14356a2b22d9eae54fdfc75d8303603b4af
Bilan Vadim <vadimb892@gmail.com>
2025-08-13 18:36:02 +0300

    feat: add new mod

---
+++ module/helper/animation/src/skeletal_animation.rs
@@ -0,0 +1,23 @@
+//!
+
+mod private
+{
+  use minwebgl as gl;
+
+  use renderer::webgl::
+  {
+    Object3D,
+    Node,
+    Scene
+  };
+
+  
+}
+
+crate::mod_interface!
+{
+  orphan use
+  {
+    
+  };
+}
