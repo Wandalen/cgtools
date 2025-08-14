f5a1a54a2e8bd6b11bfcaab80476c6fe4a12b3a2
Bilan Vadim <vadimb892@gmail.com>
2025-08-14 09:44:32 +0300

    feat: add new structs

--- module/helper/animation/src/skeletal_animation.rs
+++ module/helper/animation/src/skeletal_animation.rs
@@ -4,7 +4,8 @@ mod private
 {
   use minwebgl as gl;
 
-  use gltf
+  use gl::F32x4x4;
+  use std::{ cell::RefCell, rc::Rc };
   use renderer::webgl::
   {
     Object3D,
@@ -12,7 +13,50 @@ mod private
     Scene
   };
 
-  fn 
+  struct Rigs
+  {
+    rigs : Vec< ( Rig, Vec< Animation > ) >
+  }
+
+  // Scene analog
+  pub struct Rig
+  {
+    // Contains global matrix, local matrix and joints, weights attributes
+    root : Rc< RefCell< Node > >,
+    joint_matrices : Vec< F32x4x4 >
+  }
+
+  impl Rig
+  {
+    fn a( : Rc< RefCell< Node > > )
+    {
+
+    }
+  }
+
+  pub struct Animation
+  {
+    sequencer : Sequencer,
+  }
+
+  impl Animation
+  {
+    fn a( : Rc< RefCell< Node > > )
+    {
+
+    }
+  }
+
+  pub async fn load
+  (
+    document : &gl::web_sys::Document,
+    gltf_path : &str,
+    gl : &gl::WebGl2RenderingContext
+  ) 
+  -> Result< Rigs, gl::WebglError >
+  {
+
+  }
 }
 
 crate::mod_interface!
