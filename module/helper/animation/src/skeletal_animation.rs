c53dc6a776bc134e43be37f169d7d30eb91b1d67
Bilan Vadim <vadimb892@gmail.com>
2025-08-14 15:36:10 +0300

    feat: add rig loading (draft)

--- module/helper/animation/src/skeletal_animation.rs
+++ module/helper/animation/src/skeletal_animation.rs
@@ -2,6 +2,7 @@
 
 mod private
 {
+  use bytemuck::cast_slice;
   use minwebgl as gl;
 
   use gl::F32x4x4;
@@ -13,39 +14,36 @@ mod private
     Scene
   };
 
-  pub struct Rigs
+  pub struct Skeletons
   {
     gl_buffers : Vec< WebGlBuffer >, 
-    pub rigs : Vec< ( Rig, Vec< Sequencer > ) >
+    pub Skeletons : Vec< Skeleton >,
+    pub animations : Vec< Sequencer >
   }
 
-  impl Rigs
+  impl Skeletons
   {
     fn new() -> Self
     {
       Self
       {
         gl_buffers : vec![],
-        rigs : vec![]
+        skeletons : vec![]
       }
     }
   }
 
   // Scene analog
-  pub struct Rig
+  pub struct Skeleton
   {
     // Contains global matrix, local matrix and joints, weights attributes
     root : Rc< RefCell< Node > >,
-    joint_matrices : Vec< F32x4x4 >
+    inverse_bind_matrices :  Vec< F32x4x4 >, //HashMap< String, F32x4x4 >
+    offset : usize
   }
 
-  impl Rig
+  impl Skeleton
   {
-    fn new(  ) -> Self
-    {
-
-    }
-
     fn a( : Rc< RefCell< Node > > )
     {
 
@@ -58,7 +56,7 @@ mod private
     gltf_path : &str,
     gl : &gl::WebGl2RenderingContext
   ) 
-  -> Result< Rigs, gl::WebglError >
+  -> Result< Skeletons, gl::WebglError >
   {
     let path = std::path::Path::new( gltf_path );
     let folder_path = path.parent().map_or( "", | p | p.to_str().expect( "Path is not UTF-8 encoded" ) );
@@ -213,6 +211,7 @@ mod private
     }
 
     let mut nodes = Vec::new();
+    let mut skeletons = Vec::new();
     for gltf_node in gltf_file.nodes()
     {
       let mut node = Node::default();
@@ -232,7 +231,65 @@ mod private
       node.set_rotation( gl::QuatF32::from( rotation ) );
       if let Some( name ) = gltf_node.name() { node.set_name( name ); }
 
-      nodes.push( Rc::new( RefCell::new( node ) ) );
+      let node = Rc::new( RefCell::new( node ) );
+
+      if let Some( skin ) = gltf_node.skin()
+      {
+        if let Some( acc ) = skin.inverse_bind_matrices()
+        {
+          let mut matrices = vec![];
+
+          let view = acc.view();
+          let gltf_buffer = view.buffer();
+          let mut matrix_buffer_slice : Option< &[ [ f32; 16 ] ] > = None;
+          match gltf_buffer.source()
+          {
+            gltf::buffer::Source::Uri( uri ) =>
+            {
+              let path = format!( "{}/{}", folder_path, uri );
+              let buffer = gl::file::load( &path ).await
+              .expect( "Failed to load a buffer" );
+
+              let vl = view.length();
+              let vo = view.offset();
+              let ac = acc.count();
+              let ao = acc.offset();
+
+              matrix_buffer_slice : &[ [ f32; 16 ] ] = Some
+              ( 
+                cast_slice( buffer.as_slice()[ vo..( vo + vl ) ][ ao..( ao + ( 16 * 4 * ac ) ) ] ) 
+              );
+            },
+            _ => {}
+          }
+
+          if let Some( slice ) = matrix_buffer_slice
+          {
+            matrices = slice.into_iter()
+            .map( | array | F32x4x4::from_column_major( array ) )
+            .collect::< Vec< _ > >();
+          }
+
+          // let mut name_to_matrix = HashMap::new();
+          // let mut inverse_bind_matrices = HashMap::new();
+          // for ( joint, matrix ) in skin.joints().zip( matrices )
+          // {
+          //   if let Some( name ) = joint.name()
+          //   {  
+          //     inverse_bind_matrices.insert( name.to_string(), matrix );
+          //   }
+          // }
+
+          let skeleton = Skeleton
+          {
+            root : node.clone(),
+            inverse_bind_matrices : matrices,
+            offset : skin.joints()
+          }
+        }
+      }
+
+      nodes.push( node );
     }
 
     for gltf_node in gltf_file.nodes()
@@ -244,7 +301,7 @@ mod private
       }
     }
 
-    let mut rigs = Rigs::new();
+    let mut skeletons = Skeletons::new();
 
     for gltf_scene in gltf_file.scenes()
     {
@@ -256,7 +313,7 @@ mod private
       scenes.push(  Rc::new( RefCell::new( scene ) ) );
     }
 
-    Ok( rigs )
+    Ok( skeletons )
   }
 }
 
