025976c98b3e411c434119132f4f51a1c41ad0a7
Bilan Vadim <vadimb892@gmail.com>
2025-08-21 17:38:48 +0300

    feat: add new animatable type

--- module/helper/animation/src/interpolation.rs
+++ module/helper/animation/src/interpolation.rs
@@ -381,19 +381,7 @@ mod private
     }
   }
 
-  #[ derive( Debug, Clone ) ]
-  pub struct Translation( F32x3 );
-  
-  #[ derive( Debug, Clone ) ]
-  pub struct Rotation( QuatF32 );
-  
-  #[ derive( Debug, Clone ) ]
-  pub struct Scale( F32x3 );
-
-  // #[ derive( Debug, Clone ) ]
-  // struct Weights( ? );
-
-  impl Animatable for Translation
+  impl Animatable for F32x3
   {
     fn interpolate(&self, other : &Self, t : f32 ) -> Self 
     {
@@ -402,43 +390,115 @@ mod private
         F32x3::from
         (
           [
-            self.0.x().interpolate( &other.0.x(), t ),
-            self.0.y().interpolate( &other.0.y(), t ),
-            self.0.z().interpolate( &other.0.z(), t )
+            self.x().interpolate( &other.x(), t ),
+            self.y().interpolate( &other.y(), t ),
+            self.z().interpolate( &other.z(), t )
           ]
         )
       )
     }
   } 
 
-  impl Animatable for Rotation
+  impl Animatable for QuatF32
   {
     fn interpolate( &self, other : &Self, t : f32 ) -> Self 
     {
       Self
       (
-        self.0.slerp( &other.0, t )
+        self.slerp( &other, t )
       )
     }
   } 
 
-  impl Animatable for Scale
+  #[ derive( Debug, Clone ) ]
+  pub struct Transform
+  {
+    pub translation : Option< F32x3 >,
+    pub rotation : Option< QuatF32 >,
+    pub scale : Option< F32x3 >
+  }
+
+  impl Animatable for Transform
   {
     fn interpolate(&self, other : &Self, t : f32 ) -> Self 
     {
+      let translation = match ( self.translation, other.translation )
+      {
+        ( None, b ) => b,
+        ( a, None ) => a,
+        ( Some( a ), Some( b ) ) => a.interpolate( &b, t )
+      };
+
+      let rotation = match ( self.rotation, other.rotation )
+      {
+        ( None, b ) => b,
+        ( a, None ) => a,
+        ( Some( a ), Some( b ) ) => a.interpolate( &b, t )
+      };
+
+      let scale = match ( self.scale, other.scale )
+      {
+        ( None, b ) => b,
+        ( a, None ) => a,
+        ( Some( a ), Some( b ) ) => a.interpolate( &b, t )
+      };
+
       Self
-      (
-        F32x3::from
-        (
-          [
-            self.0.x().interpolate( &other.0.x(), t ),
-            self.0.y().interpolate( &other.0.y(), t ),
-            self.0.z().interpolate( &other.0.z(), t )
-          ]
-        )
-      )
+      {
+        translation,
+        rotation,
+        scale
+      }
     }
   } 
+
+  // #[ derive( Debug, Clone ) ]
+  // pub struct Translation( F32x3 );
+  
+  // #[ derive( Debug, Clone ) ]
+  // pub struct Rotation( QuatF32 );
+  
+  // #[ derive( Debug, Clone ) ]
+  // pub struct Scale( F32x3 );
+
+  // #[ derive( Debug, Clone ) ]
+  // struct Weights( ? );
+
+  // impl Animatable for Translation
+  // {
+  //   fn interpolate(&self, other : &Self, t : f32 ) -> Self 
+  //   {
+  //     Self
+  //     (
+  //       F32x3::from
+  //       (
+  //         [
+  //           self.0.x().interpolate( &other.0.x(), t ),
+  //           self.0.y().interpolate( &other.0.y(), t ),
+  //           self.0.z().interpolate( &other.0.z(), t )
+  //         ]
+  //       )
+  //     )
+  //   }
+  // } 
+
+  // impl Animatable for Scale
+  // {
+  //   fn interpolate(&self, other : &Self, t : f32 ) -> Self 
+  //   {
+  //     Self
+  //     (
+  //       F32x3::from
+  //       (
+  //         [
+  //           self.0.x().interpolate( &other.0.x(), t ),
+  //           self.0.y().interpolate( &other.0.y(), t ),
+  //           self.0.z().interpolate( &other.0.z(), t )
+  //         ]
+  //       )
+  //     )
+  //   }
+  // } 
 
   /// RGB Color for animations.
   #[ derive( Debug, Clone, Copy, PartialEq ) ]
@@ -833,6 +893,7 @@ crate::mod_interface!
   {
     AnimationState,
     Tween,
-    Animatable
+    Animatable,
+    Transform
   };
 }
