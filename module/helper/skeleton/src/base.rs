f0db9d564230d634939daf4cc995884b1ccbb432
Bilan Vadim <vadimb892@gmail.com>
2025-08-22 13:14:55 +0300

    feat: load matrices to texture (WIP)

--- module/helper/skeleton/src/base.rs
+++ module/helper/skeleton/src/base.rs
@@ -5,7 +5,12 @@ mod private
   use bytemuck::cast_slice;
   use minwebgl as gl;
 
-  use gl::{ GL, F32x4x4 };
+  use gl::{ GL, F32x4x4, U32x2 };
+  use web_sys::
+  {
+    WebGlProgram,
+    js_sys::Float32Array
+  };
   use std::{ cell::RefCell, collections::HashMap, rc::Rc };
   use animation::
   {
@@ -13,25 +18,99 @@ mod private
     sequencer::Sequencer,
   };
 
+  /// Creates data texture where every pixel is 4 float values. 
+  /// Used for packing uniform matrices array
+  fn create_texture_4f
+  ( 
+    gl : &GL,
+    data : &[ f32 ], 
+    size : U32x2, 
+    texture_id : u32 
+  ) -> Option< web_sys::WebGlTexture >
+  {
+    // let Ok( _ ) = gl.get_extension( "OES_texture_float" )
+    // else
+    // {
+    //   gl::error!( "skeleton crate: Failed to enable OES_texture_float extension" );
+    //   return None;
+    // };
+
+    let texture = gl.create_texture();
+    gl.active_texture( texture_id );
+    gl.bind_texture( GL::TEXTURE_2D, texture.as_ref() );  
+
+    // Create a Float32Array from the Rust slice
+    let js_data = Float32Array::from( data );
+
+    gl.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_array_buffer_view
+    (
+      GL::TEXTURE_2D,
+      0,
+      GL::RGBA32F as i32,
+      size.x() as i32,
+      size.y() as i32,
+      0,
+      GL::RGBA,
+      GL::FLOAT,
+      Some( &js_data ),
+    )?;
+
+    //gl.pixel_storei( GL::UNPACK_ALIGNMENT, 1 );
+
+    gl.tex_parameteri( GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32 );
+    gl.tex_parameteri( GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32 );
+
+    gl.tex_parameteri( GL::TEXTURE_2D, GL::TEXTURE_WRAP_S, GL::CLAMP_TO_EDGE as i32 );
+    gl.tex_parameteri( GL::TEXTURE_2D, GL::TEXTURE_WRAP_T, GL::CLAMP_TO_EDGE as i32 );
+
+    texture
+  }
+
   /// 
   pub struct Skeleton
   {
     nodes : Vec< Box< str > >,
     inverse_bind_matrices :  Vec< F32x4x4 >,
-    animation : Option< Rc< RefCell< Sequencer > > >
+    animation : Option< Rc< RefCell< Sequencer > > >,
+    texture_size : U32x2,
+    texture : WebGlTexture
   }
 
   impl Skeleton
   {
     /// 
-    fn new( nodes : Vec< Box< str > >, inverse_bind_matrices : Vec< F32x4x4 > ) -> Self
+    fn new
+    ( 
+      gl : &GL,
+      joints : HashMap< Box< str >, F32x4x4 >,
+      texture_id : u32
+    ) -> Option< Self >
     {
-      Self
+      let mut nodes = vec![];
+      let mut inverse_bind_matrices = vec![];
+
+      for ( name, matrix ) in joints
       {
-        nodes,
-        inverse_bind_matrices,
-        animation : None
+        nodes.push( name );
+        inverse_bind_matrices.push( matrix );
       }
+
+      let texture_size = U32x2::from_array( [ , ] );
+      let data = ;
+
+      let texture = create_texture_4f( gl, data, texture_size, texture_id );
+
+      Some
+      ( 
+        Self
+        {
+          nodes,
+          inverse_bind_matrices,
+          animation : None,
+          texture_size,
+          texture
+        }
+      )
     }
 
     /// 
@@ -53,9 +132,9 @@ mod private
     }
 
     /// 
-    fn upload( &self, gl : &GL ) 
+    fn upload( &self, gl : &GL, program : &WebGlProgram ) 
     {
-
+      
     }
   }
 }
