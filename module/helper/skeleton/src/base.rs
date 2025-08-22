4d476206c53e0b89baeca740f8a612488f2d73f9
Bilan Vadim <vadimb892@gmail.com>
2025-08-22 15:55:01 +0300

    feat: add docs and remove animation from Skeleton

--- module/helper/skeleton/src/base.rs
+++ module/helper/skeleton/src/base.rs
@@ -2,14 +2,18 @@
 
 mod private
 {
-  use bytemuck::cast_slice;
   use minwebgl as gl;
-
-  use gl::{ GL, F32x4x4, U32x2 };
-  use web_sys::
-  {
-    WebGlProgram,
-    js_sys::Float32Array
+  use gl::
+  { 
+    web_sys::
+    {
+      WebGlProgram,
+      js_sys::Float32Array
+    },
+    GL, 
+    F32x4x4, 
+    U32x2,
+    WebGlUniformLocation
   };
   use std::{ cell::RefCell, collections::HashMap, rc::Rc };
   use animation::
@@ -24,8 +28,7 @@ mod private
   ( 
     gl : &GL,
     data : &[ f32 ], 
-    size : U32x2, 
-    texture_id : u32 
+    size : U32x2
   ) -> Option< web_sys::WebGlTexture >
   {
     // let Ok( _ ) = gl.get_extension( "OES_texture_float" )
@@ -36,7 +39,7 @@ mod private
     // };
 
     let texture = gl.create_texture();
-    gl.active_texture( texture_id );
+    gl.active_texture( GL::TEXTURE0 );
     gl.bind_texture( GL::TEXTURE_2D, texture.as_ref() );  
 
     // Create a Float32Array from the Rust slice
@@ -66,24 +69,61 @@ mod private
     texture
   }
 
-  /// 
+  /// Binds a texture to a texture unit and uploads its location to a uniform.
+  ///
+  /// # Arguments
+  ///
+  /// * `gl` - The WebGL2 rendering context.
+  /// * `texture` - The texture to bind.
+  /// * `location` - The uniform location in the shader for the sampler.
+  /// * `slot` - The texture unit to bind to ( e.g., `GL::TEXTURE0` ).
+  fn upload_texture
+  (
+    gl : &GL,
+    texture : &WebGlTexture,
+    location : Option< WebGlUniformLocation >,
+    slot : u32,
+  )
+  {
+    gl.active_texture( slot ); 
+    gl.bind_texture( GL::TEXTURE_2D, Some( &texture ) ); 
+    // Tell the sampler uniform in the shader which texture unit to use ( 0 for GL_TEXTURE0, 1 for GL_TEXTURE1, etc. )
+    gl.uniform1i( location.as_ref(), ( slot - GL::TEXTURE0 ) as i32 );
+  }
+
+  /// Set of virtual bones used to deform and control the 
+  /// movement of a 3D models. It's a fundamental concept 
+  /// in skeletal animation, the most common method for 
+  /// rigging and animating complex models.
   pub struct Skeleton
   {
-    nodes : Vec< Box< str > >,
+    /// List of nodes name that is part of skeleton
+    joints : Vec< Box< str > >,
+    /// List of nodes correcting matrices used in nodes 
+    /// transform for playing skeletal animations 
     inverse_bind_matrices :  Vec< F32x4x4 >,
-    animation : Option< Rc< RefCell< Sequencer > > >,
+    // /// Current binded animation that controls transform 
+    // /// of all joints in every time moment
+    // animation : Option< Rc< RefCell< Sequencer > > >,
+    /// Size of [`Skeleton::texture`]
     texture_size : U32x2,
+    /// Inverse bind matrices data texture
     texture : WebGlTexture
   }
 
   impl Skeleton
   {
+    /// Create a new [`Skeleton`] instance
+    ///
+    /// # Arguments
+    ///
+    /// * `gl` - The WebGL2 rendering context.
+    /// * `joints` - mapping names to inverse bind matrices of [`Skeleton`] nodes
     /// 
     fn new
     ( 
       gl : &GL,
-      joints : HashMap< Box< str >, F32x4x4 >,
-      texture_id : u32
+      joints : HashMap< Box< str >, F32x4x4 >
     ) -> Option< Self >
     {
       let mut nodes = vec![];
@@ -98,44 +138,60 @@ mod private
       let texture_size = U32x2::from_array( [ , ] );
       let data = ;
 
-      let texture = create_texture_4f( gl, data, texture_size, texture_id );
+      let texture = create_texture_4f( gl, data, texture_size );
 
       Some
       ( 
         Self
         {
-          nodes,
+          joints : nodes,
           inverse_bind_matrices,
-          animation : None,
+          // animation : None,
           texture_size,
           texture
         }
       )
     }
 
-    /// 
-    fn set_animation( &mut self, animation : Option< &Rc< RefCell< Sequencer > > > )
+    /// Upload inverse bind matrices texture to current shader program
+    fn upload
+    ( 
+      &self, 
+      gl : &GL, 
+      locations : HashMap< Box< str >, Option< WebGlUniformLocation > >,
+      slot : u32
+    ) 
     {
-      self.animation = animation.cloned();
-    }
-
-    /// 
-    fn get_animation( &self ) -> Option< Rc< RefCell< Sequencer > > >
-    {
-      self.animation.clone()
-    }
-
-    /// 
-    fn update( &self, t : f32 ) -> HashMap< Box< str >, Transform >
-    {
+      let inverse_matrices_loc = locations.get( "inverseMatrices" ).unwrap();
+      let texture_size_loc = locations.get( "inverseMatricesSize" ).unwrap();
       
+      upload_texture( gl, self.texture, inverse_matrices_loc.clone(), slot );
+      gl::uniform::upload( &gl, texture_size_loc.clone(), self.texture_size.as_slice() ).unwrap();
     }
 
-    /// 
-    fn upload( &self, gl : &GL, program : &WebGlProgram ) 
-    {
-      
-    }
+    // /// Set current animation for this [`Skeleton`]
+    // fn set_animation( &mut self, animation : Option< &Rc< RefCell< Sequencer > > > )
+    // {
+    //   self.animation = animation.cloned();
+    // }
+
+    // /// Return current animation of this [`Skeleton`]
+    // fn get_animation( &self ) -> Option< Rc< RefCell< Sequencer > > >
+    // {
+    //   self.animation.clone()
+    // }
+
+    // /// Update current animation and get current nodes [`Transform`]'s
+    // fn update( &self, time : f32 ) -> HashMap< Box< str >, Transform >
+    // {
+    //   let Some( animation ) = self.animation.as_ref()
+    //   else
+    //   {
+    //     return HashMap::new();
+    //   }
+
+    //   animation.borrow_mut().
+    // }
   }
 }
 
