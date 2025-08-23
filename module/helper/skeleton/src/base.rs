ce6b3d27adfa9b72068cd67ef51ad58d0d5dff34
Bilan Vadim <vadimb892@gmail.com>
2025-08-23 12:27:15 +0300

    fix: compile errors

--- module/helper/skeleton/src/base.rs
+++ module/helper/skeleton/src/base.rs
@@ -4,32 +4,32 @@ mod private
 {
   use minwebgl as gl;
   use gl::
-  { 
+  {
     web_sys::
     {
-      WebGlProgram,
-      js_sys::Float32Array
+      js_sys::Float32Array,
+      WebGlTexture
     },
-    GL, 
-    F32x4x4, 
-    U32x2,
+    GL,
+    F32x4x4,
     WebGlUniformLocation
   };
-  use std::{ cell::RefCell, collections::HashMap, rc::Rc };
-  use animation::
-  {
-    interpolation::Transform,
-    sequencer::Sequencer,
-  };
+  use std::collections::HashMap;
+  // use std::{ cell::RefCell, rc::Rc };
+  // use animation::
+  // {
+  //   interpolation::Transform,
+  //   sequencer::Sequencer,
+  // };
 
-  /// Creates data texture where every pixel is 4 float values. 
+  /// Creates data texture where every pixel is 4 float values.
   /// Used for packing uniform matrices array
   fn create_texture_4f
-  ( 
+  (
     gl : &GL,
-    data : &[ f32 ], 
-    size : U32x2
-  ) -> Option< web_sys::WebGlTexture >
+    data : &[ f32 ],
+    size : [ u32; 2 ],
+  ) -> Option< WebGlTexture >
   {
     // let Ok( _ ) = gl.get_extension( "OES_texture_float" )
     // else
@@ -40,7 +40,7 @@ mod private
 
     let texture = gl.create_texture();
     gl.active_texture( GL::TEXTURE0 );
-    gl.bind_texture( GL::TEXTURE_2D, texture.as_ref() );  
+    gl.bind_texture( GL::TEXTURE_2D, texture.as_ref() );
 
     // Create a Float32Array from the Rust slice
     let js_data = Float32Array::from( data );
@@ -50,13 +50,14 @@ mod private
       GL::TEXTURE_2D,
       0,
       GL::RGBA32F as i32,
-      size.x() as i32,
-      size.y() as i32,
+      size[ 0 ] as i32,
+      size[ 1 ] as i32,
       0,
       GL::RGBA,
       GL::FLOAT,
       Some( &js_data ),
-    )?;
+    )
+    .ok()?;
 
     //gl.pixel_storei( GL::UNPACK_ALIGNMENT, 1 );
 
@@ -85,28 +86,28 @@ mod private
     slot : u32,
   )
   {
-    gl.active_texture( slot ); 
-    gl.bind_texture( GL::TEXTURE_2D, Some( &texture ) ); 
+    gl.active_texture( slot );
+    gl.bind_texture( GL::TEXTURE_2D, Some( &texture ) );
     // Tell the sampler uniform in the shader which texture unit to use ( 0 for GL_TEXTURE0, 1 for GL_TEXTURE1, etc. )
     gl.uniform1i( location.as_ref(), ( slot - GL::TEXTURE0 ) as i32 );
   }
 
-  /// Set of virtual bones used to deform and control the 
-  /// movement of a 3D models. It's a fundamental concept 
-  /// in skeletal animation, the most common method for 
+  /// Set of virtual bones used to deform and control the
+  /// movement of a 3D models. It's a fundamental concept
+  /// in skeletal animation, the most common method for
   /// rigging and animating complex models.
   pub struct Skeleton
   {
     /// List of nodes name that is part of skeleton
-    joints : Vec< Box< str > >,
-    /// List of nodes correcting matrices used in nodes 
-    /// transform for playing skeletal animations 
-    inverse_bind_matrices :  Vec< F32x4x4 >,
-    // /// Current binded animation that controls transform 
+    _joints : Vec< Box< str > >,
+    /// List of nodes correcting matrices used in nodes
+    /// transform for playing skeletal animations
+    _inverse_bind_matrices :  Vec< F32x4x4 >,
+    // /// Current binded animation that controls transform
     // /// of all joints in every time moment
     // animation : Option< Rc< RefCell< Sequencer > > >,
     /// Size of [`Skeleton::texture`]
-    texture_size : U32x2,
+    texture_size : [ u32; 2 ],
     /// Inverse bind matrices data texture
     texture : WebGlTexture
   }
@@ -119,9 +120,9 @@ mod private
     ///
     /// * `gl` - The WebGL2 rendering context.
     /// * `joints` - mapping names to inverse bind matrices of [`Skeleton`] nodes
-    /// 
-    fn new
-    ( 
+    ///
+    pub fn new
+    (
       gl : &GL,
       joints : HashMap< Box< str >, F32x4x4 >
     ) -> Option< Self >
@@ -135,17 +136,18 @@ mod private
         inverse_bind_matrices.push( matrix );
       }
 
-      let texture_size = U32x2::from_array( [ , ] );
-      let data = ;
+      let data = Vec::with_capacity( inverse_bind_matrices.len() * 16 );
+      let a = ( data.len() as f32 ).sqrt().ceil() as u32;
+      let texture_size = [ a, a ];
 
-      let texture = create_texture_4f( gl, data, texture_size );
+      let texture = create_texture_4f( gl, data.as_slice(), texture_size ).unwrap();
 
       Some
-      ( 
+      (
         Self
         {
-          joints : nodes,
-          inverse_bind_matrices,
+          _joints : nodes,
+          _inverse_bind_matrices : inverse_bind_matrices,
           // animation : None,
           texture_size,
           texture
@@ -154,18 +156,18 @@ mod private
     }
 
     /// Upload inverse bind matrices texture to current shader program
-    fn upload
-    ( 
-      &self, 
-      gl : &GL, 
+    pub fn upload
+    (
+      &self,
+      gl : &GL,
       locations : HashMap< Box< str >, Option< WebGlUniformLocation > >,
       slot : u32
-    ) 
+    )
     {
       let inverse_matrices_loc = locations.get( "inverseMatrices" ).unwrap();
       let texture_size_loc = locations.get( "inverseMatricesSize" ).unwrap();
-      
-      upload_texture( gl, self.texture, inverse_matrices_loc.clone(), slot );
+
+      upload_texture( gl, &self.texture, inverse_matrices_loc.clone(), slot );
       gl::uniform::upload( &gl, texture_size_loc.clone(), self.texture_size.as_slice() ).unwrap();
     }
 
