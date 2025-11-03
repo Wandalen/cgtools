0e6d5c82c069a1887d8bbcfa1cf31dae95fe2c90
Bilan Vadim <vadimb892@gmail.com>
2025-11-03 17:52:38 +0200

    feat: bind interface

---
+++ examples/minwebgl/jewelry_3d_site/src/ui.rs
@@ -0,0 +1,40 @@
+use minwebgl as gl;
+use gl::wasm_bindgen::{ self, prelude::* };
+use serde::{ Serialize, Deserialize };
+
+#[ derive( Default, Serialize, Deserialize ) ]
+pub struct UiState
+{
+  #[ serde( rename = "lightMode" ) ]
+  pub light_mode : String,
+  pub diamond : String,
+  pub metal : String,
+  pub ring : u32,
+  pub changed : Vec< String >
+}
+
+pub fn get_ui_state() -> Option< UiState >
+{
+  serde_wasm_bindgen::from_value::< UiState >( _get_ui_state() ).ok()
+}
+
+pub fn is_changed() -> bool
+{
+  _is_changed().as_bool().unwrap()
+}
+
+#[ wasm_bindgen( module = "/ui.js" ) ]
+extern "C"
+{
+  #[ allow( unsafe_code ) ]
+  #[ wasm_bindgen( js_name = "getUiState" ) ]
+  fn _get_ui_state() -> JsValue;
+
+  #[ allow( unsafe_code ) ]
+  #[ wasm_bindgen( js_name = "isChanged" ) ]
+  fn _is_changed() -> JsValue;
+
+  #[ allow( unsafe_code ) ]
+  #[ wasm_bindgen( js_name = "clearChanged" ) ]
+  pub fn clear_changed();
+}
