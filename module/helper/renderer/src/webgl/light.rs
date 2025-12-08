957f06b0f3c9184ecbb7dac7cbec965422824862
Bilan Vadim <vadimb892@gmail.com>
2025-12-08 15:43:47 +0200

    feat: add LightType

--- module/helper/renderer/src/webgl/light.rs
+++ module/helper/renderer/src/webgl/light.rs
@@ -3,6 +3,31 @@ mod private
   use minwebgl as gl;
   use gl::F32x3;
 
+  /// Defines light type that supported by Renderer
+  #[ derive( Debug, PartialOrd, Ord, PartialEq, Eq, Hash ) ]
+  pub enum LightType
+  {
+    /// Point light source type
+    Point,
+    /// Directional light source type
+    Direct,
+    /// Spot light source type
+    Spot
+  }
+
+  impl std::fmt::Display for LightType
+  {
+    fn fmt( &self, f: &mut std::fmt::Formatter< '_ > ) -> std::fmt::Result
+    {
+      match self
+      {
+        LightType::Point => write!( f, "Point" ),
+        LightType::Direct => write!( f, "Direct" ),
+        LightType::Spot => write!( f, "Spot" ),
+      }
+    }
+  }
+
   /// General type for supported light types
   #[ derive( Clone ) ]
   pub enum Light
@@ -44,6 +69,7 @@ crate::mod_interface!
 {
   orphan use
   {
+    LightType,
     Light,
     PointLight,
     DirectLight
