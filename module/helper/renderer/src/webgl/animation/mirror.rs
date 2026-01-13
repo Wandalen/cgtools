5f688f29bf36e9e188942230875f398c02ea8ef3
Bilan Vadim <vadimb892@gmail.com>
2026-01-13 16:11:02 +0200

    feat: complete mirror module

--- module/helper/renderer/src/webgl/animation/mirror.rs
+++ module/helper/renderer/src/webgl/animation/mirror.rs
@@ -1,36 +1,24 @@
 mod private
 {
-  use rustc_hash::{ FxHashMap, FxHashSet };
-  use std::{ rc::Rc, cell::RefCell };
   use animation::
   {
     Tween,
     Sequence,
-    Sequencer,
-    AnimatablePlayer
+    Sequencer
   };
   use mingl as gl;
-  use gl::{ F32x3, F64x3, F64x4, QuatF32, QuatF64 };
-  use crate::webgl::
-  {
-    Node,
-    animation::
-    {
-      AnimatableComposition,
-      base::
-      {
-        TRANSLATION_PREFIX,
-        ROTATION_PREFIX,
-        SCALE_PREFIX
-      }
-    }
-  };
+  use gl::{ F32x3, QuatF32 };
+  use crate::webgl::animation::base::{ ROTATION_PREFIX, TRANSLATION_PREFIX };
 
+  /// Defines mirror plane
   #[ derive( Clone, Copy, Debug ) ]
-  enum MirrorPlane
+  pub enum MirrorPlane
   {
+    /// Mirror along XY plane
     XY,
+    /// Mirror along YZ plane
     YZ,
+    /// Mirror along XZ plane
     XZ
   }
 
@@ -53,34 +41,95 @@ mod private
           {
             if key.ends_with( TRANSLATION_PREFIX )
             {
-              if let Some( mut player ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( key.into() )
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( &key )
               {
-
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 2 ] = - player.start_value.z();
+                  player.end_value.0[ 2 ] = - player.end_value.z();
+                }
               }
             }
             else if key.ends_with( ROTATION_PREFIX )
             {
-              if let Some( mut player ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( key.into() )
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< QuatF32 > > >( &key )
               {
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 0 ] = - player.start_value.x();
+                  player.start_value.0[ 1 ] = - player.start_value.y();
+                  player.end_value.0[ 0 ] = - player.end_value.x();
+                  player.end_value.0[ 1 ] = - player.end_value.y();
 
-              }
-            }
-            else if key.ends_with( SCALE_PREFIX )
-            {
-              if let Some( mut player ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( key.into() )
-              {
+                  // let [ x, y, z ] = player.start_value.to_euler_xyz().0;
+                  // player.start_value = QuatF32::from_euler_xyz( [ x, y, z ] );
 
+                  // let [ x, y, z ] = player.end_value.to_euler_xyz().0;
+                  // player.end_value = QuatF32::from_euler_xyz( [ x, y + 180_f32.to_radians(), z ] );
+                }
               }
             }
           }
         },
         MirrorPlane::YZ =>
         {
-
+          for key in animation.keys()
+          {
+            if key.ends_with( TRANSLATION_PREFIX )
+            {
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( &key )
+              {
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 0 ] = - player.start_value.x();
+                  player.end_value.0[ 0 ] = - player.end_value.x();
+                }
+              }
+            }
+            else if key.ends_with( ROTATION_PREFIX )
+            {
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< QuatF32 > > >( &key )
+              {
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 1 ] = - player.start_value.y();
+                  player.start_value.0[ 2 ] = - player.start_value.z();
+                  player.end_value.0[ 1 ] = - player.end_value.y();
+                  player.end_value.0[ 2 ] = - player.end_value.z();
+                }
+              }
+            }
+          }
         },
         MirrorPlane::XZ =>
         {
-
+          for key in animation.keys()
+          {
+            if key.ends_with( TRANSLATION_PREFIX )
+            {
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< F32x3 > > >( &key )
+              {
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 1 ] = - player.start_value.y();
+                  player.end_value.0[ 1 ] = - player.end_value.y();
+                }
+              }
+            }
+            else if key.ends_with( ROTATION_PREFIX )
+            {
+              if let Some( sequence ) = animation.get_mut::< Sequence< Tween< QuatF32 > > >( &key )
+              {
+                for player in sequence.players_mut()
+                {
+                  player.start_value.0[ 0 ] = - player.start_value.x();
+                  player.start_value.0[ 2 ] = - player.start_value.z();
+                  player.end_value.0[ 0 ] = - player.end_value.x();
+                  player.end_value.0[ 2 ] = - player.end_value.z();
+                }
+              }
+            }
+          }
         }
       }
 
