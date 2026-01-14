cc98d62e5a51dc55ef08f9b386abf99655af8a06
Bilan Vadim <vadimb892@gmail.com>
2026-01-14 10:56:44 +0200

    feat: add animation graph and mirror tests (WIP)

---
+++ module/helper/renderer/tests/animation_graph_tests.rs
@@ -0,0 +1,57 @@
+//! Tests for animation composer AnimationGraph
+#![ cfg( feature = "animation" ) ]
+
+#[ test ]
+fn animation_graph_clone_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_transition_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_transition_many_choices_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_conditions_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_remove_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_node_add_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_node_remove_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_edge_add_test()
+{
+
+}
+
+#[ test ]
+fn animation_graph_edge_remove_test()
+{
+
+}
+
---
+++ module/helper/renderer/tests/mirror_tests.rs
@@ -0,0 +1,20 @@
+//! Tests for animation modifier Mirror
+#![ cfg( feature = "animation" ) ]
+
+#[ test ]
+fn transition_mirroring_test()
+{
+
+}
+
+#[ test ]
+fn rotation_mirroring_test()
+{
+
+}
+
+#[ test ]
+fn scale_mirroring_test()
+{
+
+}
