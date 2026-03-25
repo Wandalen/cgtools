300df833cb7e6da682650186fbd097ec9d806857
Aleksandr <61507854+FairlyBet@users.noreply.github.com>
2026-03-25 14:54:48 +0200

    remove adapters to add them in separate PRs

--- module/helper/tilemap_renderer/src/adapters/mod.rs
+++ module/helper/tilemap_renderer/src/adapters/mod.rs
@@ -2,15 +2,9 @@
 
 #[ cfg( feature = "adapter-svg" ) ]
 mod svg;
-#[ cfg( feature = "adapter-svg" ) ]
-pub use svg::SvgBackend;
 
 #[ cfg( feature = "adapter-terminal" ) ]
 mod terminal;
-#[ cfg( feature = "adapter-terminal" ) ]
-pub use terminal::TerminalBackend;
 
 #[ cfg( feature = "adapter-webgl" ) ]
 mod webgl;
-#[ cfg( feature = "adapter-webgl" ) ]
-pub use webgl::WebGlBackend;
